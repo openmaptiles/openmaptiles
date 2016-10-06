8d443df89057979c3a6872aa8ada0c4dc1ccf50a
lukasmartinelli <me@lukasmartinelli.ch>
2016-10-06 19:10:00 +0200

    Classify road

--- schema/layers/road.sql
+++ schema/layers/road.sql
@@ -1,9 +1,22 @@
+CREATE OR REPLACE FUNCTION ne_road_class(type VARCHAR) RETURNS VARCHAR AS $$
+  SELECT CASE type
+	WHEN 'Major Highway' THEN 'motorway'
+	WHEN 'Secondary Highway' THEN 'trunk'
+	WHEN 'Road' THEN 'primary'
+    ELSE type
+  END;
+$$ LANGUAGE SQL IMMUTABLE;
+
 CREATE OR REPLACE VIEW ne_10m_global_roads AS (
-    SELECT geom, scalerank, type
-    FROM ne_10m_roads WHERE continent <> 'North America' AND featurecla = 'Road'
+    SELECT geom, scalerank, ne_road_class(type) AS class
+    FROM ne_10m_roads
+	WHERE continent <> 'North America'
+      AND featurecla = 'Road'
+      AND type IN ('Major Highway', 'Secondary Highway', 'Road')
     UNION ALL
-    SELECT geom, scalerank, type
+    SELECT geom, scalerank, ne_road_class(type) AS class
     FROM ne_10m_roads_north_america
+	WHERE type IN ('Major Highway', 'Secondary Highway', 'Road')
 );
 
 CREATE OR REPLACE VIEW road_z4 AS (
