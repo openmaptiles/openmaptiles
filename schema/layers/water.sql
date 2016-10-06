4f443607caa2a01d7d498b659a59373ba0b5eaa8
lukasmartinelli <me@lukasmartinelli.ch>
2016-10-06 19:10:00 +0200

    Add rivers to water

--- schema/layers/water.sql
+++ schema/layers/water.sql
@@ -16,14 +16,29 @@ CREATE OR REPLACE VIEW water_z2 AS (
     SELECT geom FROM ne_110m_lakes
 );
 
+CREATE OR REPLACE VIEW water_z3 AS (
+    SELECT geom FROM ne_50m_ocean
+    UNION ALL
+    SELECT geom FROM ne_110m_lakes
+    UNION ALL
+    SELECT geom FROM ne_110m_rivers_lake_centerlines
+    WHERE featurecla = 'River'
+);
+
 CREATE OR REPLACE VIEW water_z4 AS (
     SELECT geom FROM ne_10m_ocean
     UNION ALL
     SELECT geom FROM ne_50m_lakes
+    UNION ALL
+    SELECT geom FROM ne_50m_rivers_lake_centerlines
+    WHERE featurecla = 'River'
 );
 
 CREATE OR REPLACE VIEW water_z5 AS (
     SELECT geom FROM ne_10m_ocean
     UNION ALL
     SELECT geom FROM ne_10m_lakes
+    UNION ALL
+    SELECT geom FROM ne_10m_rivers_lake_centerlines
+    WHERE featurecla = 'River'
 );
