b0de0577922f90383c12523a4e54c54b33b70b44
lukasmartinelli <me@lukasmartinelli.ch>
2016-10-06 19:10:00 +0200

    Add water from OSM

--- schema/layers/water.sql
+++ schema/layers/water.sql
@@ -51,3 +51,27 @@ CREATE OR REPLACE VIEW water_z6 AS (
     SELECT geom FROM ne_10m_rivers_lake_centerlines
     WHERE featurecla = 'River'
 );
+
+CREATE OR REPLACE VIEW water_z8 AS (
+    SELECT way AS geom FROM water_areas
+    WHERE way_area > 100000
+);
+
+CREATE OR REPLACE VIEW water_z11 AS (
+    SELECT way AS geom FROM water_areas
+    WHERE way_area > 50000
+);
+
+CREATE OR REPLACE VIEW water_z12 AS (
+    SELECT way AS geom FROM water_areas
+    WHERE way_area > 40000
+);
+
+CREATE OR REPLACE VIEW water_z13 AS (
+    SELECT way AS geom FROM water_areas
+    WHERE way_area > 2000
+);
+
+CREATE OR REPLACE VIEW water_z14 AS (
+    SELECT way AS geom FROM water_areas
+);
