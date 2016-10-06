b4f5d94a8202bc903e775f9b9cb101db45440b6f
lukasmartinelli <me@lukasmartinelli.ch>
2016-10-06 19:10:00 +0200

    Add roads from OSM

--- schema/layers/road.sql
+++ schema/layers/road.sql
@@ -20,25 +20,66 @@ CREATE OR REPLACE VIEW ne_10m_global_roads AS (
 );
 
 CREATE OR REPLACE VIEW road_z4 AS (
-    SELECT *
+    SELECT geom, class
     FROM ne_10m_global_roads
     WHERE scalerank <= 5
 );
 
 CREATE OR REPLACE VIEW road_z5 AS (
-    SELECT *
+    SELECT geom, class
     FROM ne_10m_global_roads
     WHERE scalerank <= 6
 );
 
 CREATE OR REPLACE VIEW road_z6 AS (
-    SELECT *
+    SELECT geom, class
     FROM ne_10m_global_roads
     WHERE scalerank <= 7
 );
 
 CREATE OR REPLACE VIEW road_z7 AS (
-    SELECT *
+    SELECT geom, class
     FROM ne_10m_global_roads
     WHERE scalerank <= 7
+);
+
+CREATE OR REPLACE VIEW road_z8 AS (
+    SELECT way AS geom, class::text
+    FROM roads
+    WHERE class IN ('motorway','trunk')
+);
+
+CREATE OR REPLACE VIEW road_z9 AS (
+    SELECT way AS geom, class::text
+    FROM roads
+    WHERE class IN ('motorway','trunk', 'primary')
+);
+
+CREATE OR REPLACE VIEW road_z10 AS (
+    SELECT way AS geom, class::text
+    FROM roads
+    WHERE class IN ('motorway','trunk', 'primary', 'secondary')
+);
+
+CREATE OR REPLACE VIEW road_z11 AS (
+    SELECT way AS geom, class::text
+    FROM roads
+    WHERE class IN ('motorway','trunk', 'primary', 'secondary', 'tertiary')
+);
+
+CREATE OR REPLACE VIEW road_z12 AS (
+    SELECT way AS geom, class::text
+    FROM roads
+    WHERE class IN ('motorway','trunk','primary', 'secondary', 'tertiary', 'minor')
+);
+
+CREATE OR REPLACE VIEW road_z13 AS (
+    SELECT way AS geom, class::text
+    FROM roads
+    WHERE class NOT IN ('path')
+);
+
+CREATE OR REPLACE VIEW road_z14 AS (
+    SELECT way AS geom, class::text
+    FROM roads
 );
