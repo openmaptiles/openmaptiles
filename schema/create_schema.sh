211c765c02473e76064f13865c80dad87f70386d
lukasmartinelli <me@lukasmartinelli.ch>
2016-10-06 07:45:00 +0200

    Add water as first example layer

---
+++ schema/create_schema.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+function exec_psql_file() {
+    local file_name="$1"
+    PGPASSWORD="$POSTGRES_PASSWORD" psql \
+        -v ON_ERROR_STOP="1" \
+        --host="$POSTGRES_HOST" \
+        --port="$POSTGRES_PORT" \
+        --dbname="$POSTGRES_DB" \
+        --username="$POSTGRES_USER" \
+        -f "$file_name"
+}
+
+function main() {
+    exec_psql_file "$VT_UTIL_DIR/postgis-vt-util.sql"
+    exec_psql_file "layers/water.sql"
+}
+
+main
---
+++ schema/layers/water.sql
@@ -0,0 +1,29 @@
+CREATE OR REPLACE VIEW water_z0 AS (
+    SELECT geom FROM ne_110m_ocean
+    UNION ALL
+    SELECT geom FROM ne_110m_lakes
+);
+
+CREATE OR REPLACE VIEW water_z1 AS (
+    SELECT geom FROM ne_110m_ocean
+    UNION ALL
+    SELECT geom FROM ne_110m_lakes
+);
+
+CREATE OR REPLACE VIEW water_z2 AS (
+    SELECT geom FROM ne_50m_ocean
+    UNION ALL
+    SELECT geom FROM ne_110m_lakes
+);
+
+CREATE OR REPLACE VIEW water_z4 AS (
+    SELECT geom FROM ne_10m_ocean
+    UNION ALL
+    SELECT geom FROM ne_50m_lakes
+);
+
+CREATE OR REPLACE VIEW water_z5 AS (
+    SELECT geom FROM ne_10m_ocean
+    UNION ALL
+    SELECT geom FROM ne_10m_lakes
+);
