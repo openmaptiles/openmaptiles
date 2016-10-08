f8ddbfd1b0d2befcea729bd6330d6b4ec860db34
lukasmartinelli <me@lukasmartinelli.ch>
2016-10-08 16:42:16 +0200

    Refactor ice into table function

--- schema/layers/ice.sql
+++ schema/layers/ice.sql
@@ -15,3 +15,19 @@ CREATE OR REPLACE VIEW ice_z5 AS (
     UNION ALL
     SELECT geom, 'ice_shelf' AS type FROM ne_10m_antarctic_ice_shelves_polys
 );
+
+CREATE OR REPLACE FUNCTION layer_ice(bbox geometry, zoom_level int)
+RETURNS TABLE(geom geometry, class text) AS $$
+    WITH zoom_levels AS (
+        SELECT ST_Simplify(geom, 80000) AS geom, type FROM ice_z0
+        WHERE zoom_level BETWEEN 0 AND 1
+        UNION ALL
+        SELECT * FROM ice_z2
+        WHERE zoom_level BETWEEN 2 AND 4
+        UNION ALL
+        SELECT * FROM ice_z5
+        WHERE zoom_level BETWEEN 5 AND 8
+    )
+    SELECT geom, type::text AS class FROM zoom_levels
+    WHERE geom && bbox;
+$$ LANGUAGE SQL IMMUTABLE;
