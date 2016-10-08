402242e2c4b67cc2213003d343cd278d78207940
lukasmartinelli <me@lukasmartinelli.ch>
2016-10-08 16:47:24 +0200

    Refactor building into table function

--- schema/layers/building.sql
+++ schema/layers/building.sql
@@ -5,3 +5,20 @@ CREATE OR REPLACE VIEW building_z13 AS (
 CREATE OR REPLACE VIEW building_z14 AS (
     SELECT osm_id, way, height, levels FROM buildings
 );
+
+CREATE OR REPLACE FUNCTION layer_building(bbox geometry, zoom_level int)
+RETURNS TABLE(geom geometry, osm_id bigint, render_height float) AS $$
+    WITH zoom_levels AS (
+        SELECT osm_id, ST_Simplify(way, 10) AS way, height, levels FROM building_z13
+        WHERE zoom_level = 13
+        UNION ALL
+        SELECT * FROM building_z14
+        WHERE zoom_level >= 14
+    )
+    SELECT way, osm_id,
+    least(greatest(3, COALESCE(height, levels*3.66,5)),400)^.7 AS render_height
+    FROM zoom_levels
+    WHERE way && bbox
+    ORDER BY render_height, ST_YMin(way) DESC;
+$$ LANGUAGE SQL IMMUTABLE;
+
