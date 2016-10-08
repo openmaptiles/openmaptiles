c4ddad9d9af429479d6b5e1014df6617c5cc4fc7
lukasmartinelli <me@lukasmartinelli.ch>
2016-10-08 14:28:46 +0200

    Show ice shelves from z0

--- schema/layers/ice.sql
+++ schema/layers/ice.sql
@@ -1,5 +1,7 @@
 CREATE OR REPLACE VIEW ice_z0 AS (
     SELECT geom, 'glacier' AS type FROM ne_110m_glaciated_areas
+    UNION ALL
+    SELECT geom, 'ice_shelf' AS type FROM ne_50m_antarctic_ice_shelves_polys
 );
 
 CREATE OR REPLACE VIEW ice_z2 AS (
