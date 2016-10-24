f3b316a3009570ef3fd4af3277a84cf71c1035e9
lukasmartinelli <me@lukasmartinelli.ch>
2016-10-24 15:44:09 +0200

    Round render height as int

--- layers/building/building.sql
+++ layers/building/building.sql
@@ -9,7 +9,7 @@ CREATE OR REPLACE VIEW building_z14 AS (
 CREATE OR REPLACE FUNCTION layer_building(bbox geometry, zoom_level int)
 RETURNS TABLE(geom geometry, osm_id bigint, render_height numeric) AS $$
     SELECT geometry, osm_id,
-    least(greatest(3, COALESCE(height, levels*3.66,5)),400)^.7 AS render_height
+    least(greatest(3, COALESCE(height, levels*3.66,5)),400)^.7::int AS render_height
     FROM (
         SELECT osm_id, ST_Simplify(geometry, 10) AS geometry, height, levels FROM building_z13
         WHERE zoom_level = 13 AND geometry && bbox
@@ -17,6 +17,6 @@ RETURNS TABLE(geom geometry, osm_id bigint, render_height numeric) AS $$
         SELECT * FROM building_z14
         WHERE zoom_level >= 14 AND geometry && bbox
     ) AS zoom_levels
-    ORDER BY render_height, ST_YMin(geometry) DESC;
+    ORDER BY render_height ASC, ST_YMin(geometry) DESC;
 $$ LANGUAGE SQL IMMUTABLE;
 
