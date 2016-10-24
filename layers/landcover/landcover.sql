2fdfa8aad29af19d2d460e2b4425d9ff3fc244bd
lukasmartinelli <me@lukasmartinelli.ch>
2016-10-24 09:51:19 +0200

    Integrate ice into landcover

--- layers/landcover/landcover.sql
+++ layers/landcover/landcover.sql
@@ -1,3 +1,20 @@
+CREATE OR REPLACE VIEW landcover_z0 AS (
+    SELECT NULL::int AS osm_id, geom AS geometry, 'glacier' AS landuse, NULL AS "natural", NULL AS wetland FROM ne_110m_glaciated_areas
+);
+
+CREATE OR REPLACE VIEW landcover_z2 AS (
+    SELECT NULL::int AS osm_id, geom AS geometry, 'glacier' AS landuse, NULL AS "natural", NULL AS wetland FROM ne_50m_glaciated_areas
+    UNION ALL
+    SELECT NULL::int AS osm_id, geom AS geometry, 'ice_shelf' AS landuse, NULL AS "natural", NULL AS wetland FROM ne_50m_antarctic_ice_shelves_polys
+);
+
+CREATE OR REPLACE VIEW landcover_z5 AS (
+    SELECT NULL::int AS osm_id, geom AS geometry, 'glacier' AS landuse, NULL AS "natural", NULL AS wetland FROM ne_10m_glaciated_areas
+    UNION ALL
+    SELECT NULL::int AS osm_id, geom AS geometry, 'ice_shelf' AS landuse, NULL AS "natural", NULL AS wetland FROM ne_10m_antarctic_ice_shelves_polys
+);
+
+
 CREATE OR REPLACE VIEW landcover_z8 AS (
     SELECT osm_id, geometry, landuse, "natural", wetland FROM osm_landcover_polygon
     WHERE ST_Area(geometry) > 15000000
@@ -32,6 +49,15 @@ CREATE OR REPLACE VIEW landcover_z14 AS (
 CREATE OR REPLACE FUNCTION layer_landcover(bbox geometry, zoom_level int)
 RETURNS TABLE(osm_id bigint, geom geometry, landuse text, "natural" text, wetland text) AS $$
     SELECT osm_id, geometry, landuse, "natural", wetland FROM (
+        SELECT * FROM landcover_z0
+        WHERE zoom_level BETWEEN 0 AND 1 AND geometry && bbox
+        UNION ALL
+        SELECT * FROM landcover_z2
+        WHERE zoom_level BETWEEN 2 AND 4 AND geometry && bbox
+        UNION ALL
+        SELECT * FROM landcover_z5
+        WHERE zoom_level BETWEEN 5 AND 7 AND geometry && bbox
+        UNION ALL
         SELECT osm_id, ST_Simplify(geometry, 300) AS geometry, landuse, "natural", wetland
         FROM landcover_z8 WHERE zoom_level = 8 AND geometry && bbox
         UNION ALL
