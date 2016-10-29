001e44ccd748e161e8591ad479ed1c10d4ae96e6
lukasmartinelli <me@lukasmartinelli.ch>
2016-10-29 11:26:35 +0200

    Increase poi buffer and select correct name_en from place

--- layers/place/place.sql
+++ layers/place/place.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE FUNCTION layer_place(bbox geometry, zoom_level int, pixel_width numeric)
 RETURNS TABLE(osm_id bigint, geometry geometry, name text, name_en text, class text, "rank" int) AS $$
-    SELECT osm_id, geometry, name, name AS name_en, 'country' AS class, "rank" FROM layer_country(bbox, zoom_level)
+    SELECT osm_id, geometry, name, name_en, 'country' AS class, "rank" FROM layer_country(bbox, zoom_level)
     UNION ALL
     SELECT osm_id, geometry, name, name_en, 'state' AS class, "rank" FROM layer_state(bbox, zoom_level)
     UNION ALL
