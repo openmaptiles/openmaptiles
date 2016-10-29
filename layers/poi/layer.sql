ab4413e2093c1d49e4003bf98281a4e766c83f71
lukasmartinelli <me@lukasmartinelli.ch>
2016-10-29 10:57:06 +0200

    Gridrank should be int

--- layers/poi/layer.sql
+++ layers/poi/layer.sql
@@ -1,10 +1,10 @@
 CREATE OR REPLACE FUNCTION layer_poi(bbox geometry, zoom_level integer, pixel_width numeric)
-RETURNS TABLE(osm_id bigint, geometry geometry, name text, name_en text, class text, subclass text, gridrank bigint) AS $$
-    SELECT id, geometry, name, NULLIF(name_en, ''), poi_class(subclass) AS class, subclass,
+RETURNS TABLE(osm_id bigint, geometry geometry, name text, name_en text, class text, subclass text, gridrank int) AS $$
+    SELECT osm_id, geometry, name, NULLIF(name_en, ''), poi_class(subclass) AS class, subclass,
         row_number() OVER (
             PARTITION BY LabelGrid(geometry, 100 * pixel_width)
             ORDER BY poi_class_rank(poi_class(subclass)) ASC, length(name) DESC
-        ) AS gridrank
+        )::int AS gridrank
     FROM osm_poi_point
     WHERE geometry && bbox
       AND zoom_level >= 14
