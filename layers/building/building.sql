424a8499398405b5569208d867be5cf7ee5ed996
stirringhalo <stirringhalo@users.noreply.github.com>
2016-11-15 17:37:25 -0500

    Add better support for 3d buildings
    Change key from "levels" to "building:levels" as the "levels" key is non-standard and sees much less use than "building:levels"

--- layers/building/building.sql
+++ layers/building/building.sql
@@ -7,15 +7,16 @@
 CREATE OR REPLACE FUNCTION layer_building(bbox geometry, zoom_level int)
 RETURNS TABLE(geom geometry, osm_id bigint, render_height numeric) AS $$
     SELECT geometry, osm_id,
-    least(greatest(3, COALESCE(height, levels*3.66,5)),400)^.7::int AS render_height
+    least(greatest(3, COALESCE(height, levels*3.66,5)),400)^.7::int AS render_height,
+    least(greatest(0, COALESCE(min_height, min_levels*3.66,5)),400)^.7::int AS render_min_height
     FROM (
 
         -- etldoc: osm_building_polygon_gen1 -> layer_building:z13
-        SELECT osm_id, geometry, height, levels FROM osm_building_polygon_gen1
+        SELECT osm_id, geometry, height, levels, min_height, min_levels FROM osm_building_polygon_gen1
         WHERE zoom_level = 13 AND geometry && bbox AND area > 1400
         UNION ALL
         -- etldoc: osm_building_polygon -> layer_building:z14_
-        SELECT osm_id, geometry, height, levels FROM osm_building_polygon
+        SELECT osm_id, geometry, height, levels, min_height, min_levels FROM osm_building_polygon
         WHERE zoom_level >= 14 AND geometry && bbox
     ) AS zoom_levels
     ORDER BY render_height ASC, ST_YMin(geometry) DESC;
