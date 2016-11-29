5ebb33575e7951ba9f38d5bb395aa10753d08642
Lukas Martinelli <me@lukasmartinelli.ch>
2016-11-29 19:38:02 +0000

    Move public parks indicating grass to landcover

--- layers/landcover/landcover.sql
+++ layers/landcover/landcover.sql
@@ -9,12 +9,12 @@
 --);
 --CREATE INDEX IF NOT EXISTS landcover_grouped_gen2_geometry_idx ON landcover_grouped_gen2 USING gist(geometry);
 
-CREATE OR REPLACE FUNCTION landcover_class(landuse VARCHAR, "natural" VARCHAR, wetland VARCHAR) RETURNS TEXT AS $$
+CREATE OR REPLACE FUNCTION landcover_class(landuse VARCHAR, "natural" VARCHAR, leisure VARCHAR, wetland VARCHAR) RETURNS TEXT AS $$
     SELECT CASE
          WHEN landuse IN ('farmland', 'farm', 'orchard', 'vineyard', 'plant_nursery') THEN 'farmland'
          WHEN "natural" IN ('glacier', 'ice_shelf') THEN 'ice'
          WHEN "natural"='wood' OR landuse IN ('forest', 'wood') THEN 'wood'
-         WHEN "natural"='grassland' OR landuse IN ('grass', 'meadow', 'allotments', 'grassland') THEN 'grass'
+         WHEN "natural"='grassland' OR landuse IN ('grass', 'meadow', 'allotments', 'grassland', 'park', 'village_green', 'recreation_ground') OR leisure='park' THEN 'grass'
          WHEN "natural"='wetland' OR wetland IN ('bog', 'swamp', 'wet_meadow', 'marsh', 'reedbed', 'saltern', 'tidalflat', 'saltmarsh', 'mangrove') THEN 'wetland'
         ELSE NULL
     END;
@@ -22,58 +22,58 @@ $$ LANGUAGE SQL IMMUTABLE;
 
 -- etldoc: ne_110m_glaciated_areas ->  landcover_z0 
 CREATE OR REPLACE VIEW landcover_z0 AS (
-    SELECT NULL::bigint AS osm_id, geom AS geometry, NULL::text AS landuse, 'glacier'::text AS "natural", NULL::text AS wetland FROM ne_110m_glaciated_areas
+    SELECT NULL::bigint AS osm_id, geom AS geometry, NULL::text AS landuse, 'glacier'::text AS "natural", NULL::text AS leisure, NULL::text AS wetland FROM ne_110m_glaciated_areas
 );
 
 CREATE OR REPLACE VIEW landcover_z2 AS (
     -- etldoc: ne_50m_glaciated_areas ->  landcover_z2 
-    SELECT NULL::bigint AS osm_id, geom AS geometry, NULL::text AS landuse, 'glacier'::text AS "natural", NULL::text AS wetland FROM ne_50m_glaciated_areas
+    SELECT NULL::bigint AS osm_id, geom AS geometry, NULL::text AS landuse, 'glacier'::text AS "natural", NULL::text AS leisure, NULL::text AS wetland FROM ne_50m_glaciated_areas
     UNION ALL
     -- etldoc: ne_50m_antarctic_ice_shelves_polys ->  landcover_z2     
-    SELECT NULL::bigint AS osm_id, geom AS geometry, NULL::text AS landuse, 'ice_shelf'::text AS "natural", NULL::text AS wetland FROM ne_50m_antarctic_ice_shelves_polys
+    SELECT NULL::bigint AS osm_id, geom AS geometry, NULL::text AS landuse, 'ice_shelf'::text AS "natural", NULL::text AS leisure, NULL::text AS wetland FROM ne_50m_antarctic_ice_shelves_polys
 );
 
 CREATE OR REPLACE VIEW landcover_z5 AS (
     -- etldoc: ne_10m_glaciated_areas ->  landcover_z5
-    SELECT NULL::bigint AS osm_id, geom AS geometry, NULL::text AS landuse, 'glacier'::text AS "natural", NULL::text AS wetland FROM ne_10m_glaciated_areas
+    SELECT NULL::bigint AS osm_id, geom AS geometry, NULL::text AS landuse, 'glacier'::text AS "natural", NULL::text AS leisure, NULL::text AS wetland FROM ne_10m_glaciated_areas
     UNION ALL
     -- etldoc: ne_10m_antarctic_ice_shelves_polys ->  landcover_z5 
-    SELECT NULL::bigint AS osm_id, geom AS geometry, NULL::text AS landuse, 'ice_shelf'::text AS "natural", NULL::text AS wetland FROM ne_10m_antarctic_ice_shelves_polys
+    SELECT NULL::bigint AS osm_id, geom AS geometry, NULL::text AS landuse, 'ice_shelf'::text AS "natural", NULL::text AS leisure, NULL::text AS wetland FROM ne_10m_antarctic_ice_shelves_polys
 );
 
 CREATE OR REPLACE VIEW landcover_z8 AS (
     -- etldoc: osm_landcover_polygon_gen5 ->  landcover_z8  
-    SELECT osm_id, geometry, landuse, "natural", wetland FROM osm_landcover_polygon_gen5
+    SELECT osm_id, geometry, landuse, "natural", leisure, wetland FROM osm_landcover_polygon_gen5
 );
 
 CREATE OR REPLACE VIEW landcover_z9 AS (
     -- etldoc: osm_landcover_polygon_gen4 ->  landcover_z9    
-    SELECT osm_id, geometry, landuse, "natural", wetland FROM osm_landcover_polygon_gen4
+    SELECT osm_id, geometry, landuse, "natural", leisure, wetland FROM osm_landcover_polygon_gen4
 );
 
 CREATE OR REPLACE VIEW landcover_z10 AS (
     -- etldoc: osm_landcover_polygon_gen3 ->  landcover_z10    
-    SELECT osm_id, geometry, landuse, "natural", wetland FROM osm_landcover_polygon_gen3
+    SELECT osm_id, geometry, landuse, "natural", leisure, wetland FROM osm_landcover_polygon_gen3
 );
 
 CREATE OR REPLACE VIEW landcover_z11 AS (
     -- etldoc: osm_landcover_polygon_gen2 ->  landcover_z11    
-    SELECT osm_id, geometry, landuse, "natural", wetland FROM osm_landcover_polygon_gen2
+    SELECT osm_id, geometry, landuse, "natural", leisure, wetland FROM osm_landcover_polygon_gen2
 );
 
 CREATE OR REPLACE VIEW landcover_z12 AS (
     -- etldoc: osm_landcover_polygon_gen1 ->  landcover_z12    
-    SELECT osm_id, geometry, landuse, "natural", wetland FROM osm_landcover_polygon_gen1
+    SELECT osm_id, geometry, landuse, "natural", leisure, wetland FROM osm_landcover_polygon_gen1
 );
 
 CREATE OR REPLACE VIEW landcover_z13 AS (
     -- etldoc: osm_landcover_polygon ->  landcover_z13
-    SELECT osm_id, geometry, landuse, "natural", wetland FROM osm_landcover_polygon WHERE ST_Area(geometry) > 60000
+    SELECT osm_id, geometry, landuse, "natural", leisure, wetland FROM osm_landcover_polygon WHERE ST_Area(geometry) > 60000
 );
 
 CREATE OR REPLACE VIEW landcover_z14 AS (
     -- etldoc: osm_landcover_polygon ->  landcover_z14    
-    SELECT osm_id, geometry, landuse, "natural", wetland FROM osm_landcover_polygon
+    SELECT osm_id, geometry, landuse, "natural", leisure, wetland FROM osm_landcover_polygon
 );
 
 -- etldoc: layer_landcover[shape=record fillcolor=lightpink, style="rounded,filled",  
@@ -82,7 +82,7 @@ CREATE OR REPLACE VIEW landcover_z14 AS (
 CREATE OR REPLACE FUNCTION layer_landcover(bbox geometry, zoom_level int)
 RETURNS TABLE(osm_id bigint, geometry geometry, class text, subclass text) AS $$
     SELECT osm_id, geometry,
-        landcover_class(landuse, "natural", wetland) AS class,
+        landcover_class(landuse, "natural", leisure, wetland) AS class,
         COALESCE(NULLIF("natural", ''), NULLIF(landuse, ''), NULLIF('wetland', '')) AS subclass
         FROM (
         -- etldoc:  landcover_z0 -> layer_landcover:z0
