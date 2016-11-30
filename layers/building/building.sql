f98a43ae78f69fdccc39fa2bb7798e7f4b6c9379
stirringhalo <stirringhalo@users.noreply.github.com>
2016-11-30 18:26:55 -0500

    Probably more optimized union adjacent buildings

--- layers/building/building.sql
+++ layers/building/building.sql
@@ -2,19 +2,19 @@
 -- etldoc:     label="layer_building | <z13> z13 | <z14_> z14_ " ] ;
 
 CREATE OR REPLACE FUNCTION layer_building(bbox geometry, zoom_level int)
-RETURNS TABLE(geom geometry, osm_id bigint, render_height int, render_min_height int) AS $$
-    SELECT geometry, osm_id,
+RETURNS TABLE(geom geometry, render_height int, render_min_height int) AS $$
+    SELECT geometry,
     greatest(5, COALESCE(height, levels*3.66,5))::int AS render_height,
     greatest(0, COALESCE(min_height, min_level*3.66,0))::int AS render_min_height
     FROM (
         -- etldoc: osm_building_merged_polygon_gen1 -> layer_building:z13
-        SELECT osm_id, geometry, NULL AS height, NULL AS levels,
+        SELECT geometry, NULL AS height, NULL AS levels,
         NULL AS min_height, NULL AS min_level
-        FROM osm_building_merged_polygon_gen1
+        FROM osm_buildings_large
         WHERE zoom_level = 13 AND geometry && bbox
         UNION ALL
         -- etldoc: osm_building_polygon -> layer_building:z14_
-        SELECT osm_id, geometry, height, levels, min_height, min_level FROM osm_building_polygon
+        SELECT geometry, height, levels, min_height, min_level FROM osm_building_polygon
         WHERE zoom_level >= 14 AND geometry && bbox
     ) AS zoom_levels
     ORDER BY render_height ASC, ST_YMin(geometry) DESC;
--- layers/building/merge_buildings.sql
+++ layers/building/merge_buildings.sql
@@ -1,19 +1,50 @@
 -- Merge building polygons together to have more large buildings at lower zoom levels
 
 -- etldoc: osm_building_polygon ->  osm_merged_building_polygon
--- TODO: This is just a quick hack - needs to be improved
-CREATE TABLE IF NOT EXISTS osm_building_merged_polygon_gen1 AS (
-    SELECT * FROM (
-    SELECT
-        MAX(osm_id) AS osm_id,
-        -- We first buffer the buildings so very close buildings get merged together
-        -- TODO: Perhaps we can use snap to grid here?
-        -- then we union the geometries to find out which geometries can be dissolved
-        -- and unpack and simplify them.
-        ST_Simplify((ST_Dump(ST_Union(ST_Buffer(geometry, 5)))).geom, 8) AS geometry
-      FROM osm_building_polygon
-      GROUP BY LabelGrid(geometry, 10000)
-    ) AS grouped_buildings
-    WHERE ST_Area(geometry) > 1000
-);
-CREATE INDEX IF NOT EXISTS osm_building_merged_polygon_geometry_idx ON osm_building_merged_polygon_gen1 USING gist(geometry);
+
+-- Big help from Regina Obe http://lists.osgeo.org/pipermail/postgis-users/2007-October/017205.html for the ST_ExteriorRing fix and Nicklas Aven/Brad Koch http://gis.stackexchange.com/a/4508 for the fancy ST_Intersects
+
+-- Perhaps combine all the create tables if possible?
+
+-- Some temp tables, create a buffer for buildings of the distance to decide adjacent buildings and then take the exterior ring of that buffer
+-- If you uncomment the part of the next line, you will only do adjacency tests on buildings that are smaller than 1000 to see if they have a nearby building.
+CREATE TABLE IF NOT EXISTS buffer_table AS SELECT ST_Buffer(geometry,1) AS buffer FROM osm_building_polygon; -- WHERE ST_Area(geometry) < 1000;
+CREATE TABLE IF NOT EXISTS extring_table AS SELECT ST_ExteriorRing(ST_GeometryN(buffer, generate_series(1, ST_NumGeometries(buffer)))) AS extring FROM buffer_table;
+CREATE INDEX IF NOT EXISTS extring_line_idx ON extring_table USING gist(extring);
+
+-- Find buildings that have a building adjacent within the buffer
+-- If you uncomment the ST_Area limitation, you can ensure that the only nearby buildings that are considered for adjacency are ones smaller than 1000.
+-- If you uncomment both this line and the other ST_Area limit, you will only be getting buildings that are < 1000 with adjacent buildings that are also < 1000
+CREATE TABLE IF NOT EXISTS adjacent_buildings AS 
+  SELECT DISTINCT geometry FROM osm_building_polygon AS building
+  LEFT JOIN extring_table AS extring
+  ON ST_Intersects(building.geometry,extring.extring)
+  WHERE extring.extring IS NOT NULL; -- AND ST_Area(building.geometry) < 1000;
+
+-- st_union the buildings that have adjacent buildings and dump to polygons
+-- CREATE TABLE osm_building_union AS
+--   SELECT (ST_Dump(ST_Union(geometry))).geom AS geometry
+--   FROM adjacent_buildings;
+
+-- Now, union all the big buildings with the adjacent buildings, st_union again and simplify.
+CREATE TABLE osm_buildings_large AS
+  SELECT ST_Simplify(geometry,4) AS geometry FROM 
+  (SELECT (ST_Dump(ST_Union(geometry))).geom AS geometry FROM
+  (SELECT (ST_Dump(ST_Union(geometry))).geom AS geometry
+  FROM adjacent_buildings
+  UNION ALL
+  SELECT geometry
+  FROM osm_building_polygon WHERE ST_Area(geometry) >= 1000)
+  AS all_buildings) 
+  AS union_buildings
+  WHERE ST_Area(geometry) >= 1000;	-- Ensure the resulting area before simplify is greater than 1000
+
+CREATE INDEX IF NOT EXISTS osm_buildings_large_geometry_idx ON osm_buildings_large USING gist(geometry);
+
+-- DROP TABLE IF EXISTS osm_building_union;
+DROP TABLE IF EXISTS adjacent_buildings;
+DROP TABLE IF EXISTS extring_table;
+DROP TABLE IF EXISTS buffer_table;
+
+
+
