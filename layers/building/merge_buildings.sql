a7286955180297a68053f27a3b2aa216c8c7a310
stirringhalo <stirringhalo@users.noreply.github.com>
2016-12-02 13:55:31 -0500

    Squash commits
    Do not select area
    
    >= area instead of just >
    
    no osm_id
    
    Remove gen1 table
    
    Recompute area, don't use imposm area

--- layers/building/merge_buildings.sql
+++ layers/building/merge_buildings.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS large_building_clusters AS
          geometry FROM building_clusters_touches
          WHERE cid IS NOT NULL GROUP BY cid
       ) AS stunion
-      WHERE ST_Area(geometry) > 1000
+      WHERE ST_Area(geometry) >= 1000
   );
 
 -- Union all the clusters of buildings that are large with the buildings that are isolated but large on their own
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS osm_buildings_large AS
   (
     SELECT geometry
     FROM building_clusters_touches		-- Add buildings with nothing adjacent but are larger than area already
-    WHERE cid IS NULL AND area >= 1000	-- Not sure if area is accurate enough
+    WHERE cid IS NULL AND ST_Area(geometry) >= 1000
       UNION ALL
     SELECT geometry
     FROM large_building_clusters	-- Already large enough
