681b50ff1c74eaf4afe6ec60ff67895e07715d62
Lukas Martinelli <me@lukasmartinelli.ch>
2016-12-02 12:39:10 +0000

    Remove subclass from transportation_name

--- layers/transportation_name/layer.sql
+++ layers/transportation_name/layer.sql
@@ -3,12 +3,12 @@
 -- etldoc:     label="layer_transportation_name | <z8> z8 |<z9> z9 |<z10> z10 |<z11> z11 |<z12> z12|<z13> z13|<z14_> z14_" ] ;
 
 CREATE OR REPLACE FUNCTION layer_transportation_name(bbox geometry, zoom_level integer)
-RETURNS TABLE(osm_id bigint, geometry geometry, name text, ref text, ref_length int, network text, class text, subclass text) AS $$
+RETURNS TABLE(osm_id bigint, geometry geometry, name text, ref text, ref_length int, network text, class text) AS $$
     SELECT osm_id, geometry, name,
       NULLIF(ref, ''), NULLIF(LENGTH(ref), 0) AS ref_length,
       --TODO: The road network of the road is not yet implemented
       NULL::text AS network,
-      highway_class(highway) AS class, highway AS subclass
+      highway_class(highway) AS class
     FROM (
 
         -- etldoc: osm_transportation_name_linestring_gen3 ->  layer_transportation_name:z8
