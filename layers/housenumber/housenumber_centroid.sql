b97e5ad9fbc63eacc8c1b3798a6e4980ca55dee1
stirringhalo <stirringhalo@users.noreply.github.com>
2017-01-04 17:40:52 -0500

    Convert housenumber point conversion to function

--- layers/housenumber/housenumber_centroid.sql
+++ layers/housenumber/housenumber_centroid.sql
@@ -1,4 +1,9 @@
 
 -- etldoc: osm_housenumber_point -> osm_housenumber_point
-UPDATE osm_housenumber_point SET geometry=topoint(geometry)
-WHERE ST_GeometryType(geometry) <> 'ST_Point';
+CREATE FUNCTION convert_housenumber_point() RETURNS VOID AS $$
+BEGIN
+  UPDATE osm_housenumber_point SET geometry=topoint(geometry) WHERE ST_GeometryType(geometry) <> 'ST_Point';
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT convert_housenumber_point();
