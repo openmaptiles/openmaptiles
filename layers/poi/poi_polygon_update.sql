f1ee63e4bb90cee9ea5eadd3ec79b53fdf27c926
stirringhalo <stirringhalo@users.noreply.github.com>
2017-01-04 17:42:05 -0500

    poi topoint function

--- layers/poi/poi_polygon_update.sql
+++ layers/poi/poi_polygon_update.sql
@@ -1,5 +1,11 @@
 -- etldoc:  osm_poi_polygon ->  osm_poi_polygon
-UPDATE osm_poi_polygon  SET geometry=topoint(geometry)
-WHERE ST_GeometryType(geometry) <> 'ST_Point';
 
-ANALYZE osm_poi_polygon;
+CREATE FUNCTION convert_poi_point() RETURNS VOID AS $$
+BEGIN
+  UPDATE osm_poi_polygon SET geometry=topoint(geometry) WHERE ST_GeometryType(geometry) <> 'ST_Point';
+  ANALYZE osm_poi_polygon;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT convert_poi_point();
+
