88a522846b7d80bf8dc5d6ce6a74df85c51a4bae
stirringhalo <stirringhalo@users.noreply.github.com>
2017-01-18 21:37:14 -0500

    Initial relation support for buildings

--- layers/building/building.sql
+++ layers/building/building.sql
@@ -19,6 +19,18 @@ RETURNS TABLE(geometry geometry, osm_id bigint, render_height int, render_min_he
             greatest(0, COALESCE(min_height, min_level*3.66,0))::int AS render_min_height
         FROM osm_building_polygon
         WHERE zoom_level >= 14 AND geometry && bbox
+        UNION ALL
+        -- etldoc: osm_building_polygon_member -> layer_building:z14_
+        SELECT osm_id, geometry, 
+            greatest(5, COALESCE(height, levels*3.66,5))::int AS render_height,
+            greatest(0, COALESCE(min_height, min_level*3.66,0))::int AS render_min_height
+        FROM osm_building_polygon_member
+        WHERE zoom_level >= 14 AND 
+        role <> 'outline' AND type = '1' AND
+        ST_IsClosed(geometry) = 't' AND
+        ST_NPoints(geometry) > 3
+        geometry && bbox -- see http://wiki.openstreetmap.org/wiki/Relation:building for samples. May need to subtract outline from others if it overlaps with any members?
+        -- need to handle multipolygons? Yes.
     ) AS zoom_levels
     ORDER BY render_height ASC, ST_YMin(geometry) DESC;
 $$ LANGUAGE SQL IMMUTABLE;
