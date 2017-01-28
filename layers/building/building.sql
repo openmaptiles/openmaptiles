91b6b61d1eaf67383e7de5827952ea8f364081f9
stirringhalo <stirringhalo@users.noreply.github.com>
2017-01-28 09:15:23 -0500

    Finalized
    Major breakthrough, just some artifacting to resolve

--- layers/building/building.sql
+++ layers/building/building.sql
@@ -1,6 +1,3 @@
-
-DROP MATERIALIZED VIEW IF EXISTS all_buildings CASCADE;
-
 -- etldoc: layer_building[shape=record fillcolor=lightpink, style="rounded,filled",
 -- etldoc:     label="layer_building | <z13> z13 | <z14_> z14+ " ] ;
 
@@ -16,25 +13,37 @@ END;
 $$ STRICT
 LANGUAGE plpgsql IMMUTABLE;
 
-CREATE MATERIALIZED VIEW all_buildings AS (
-        SELECT * FROM (
-	SELECT
-        obp.osm_id,
-           COALESCE(obp.geometry,obpm.geometry) AS geometry,
-     greatest(as_numeric(obp.height),as_numeric(obpm.height),as_numeric(obpm.buildingheight),as_numeric(obpm.relheight),as_numeric(obpm.relbuildingheight)) AS height, 
-     greatest(as_numeric(obp.min_height),as_numeric(obpm.min_height),as_numeric(obpm.buildingmin_height),as_numeric(obpm.relmin_height),as_numeric(obpm.relbuildingmin_height)) AS min_height,
-     greatest(as_numeric(obp.levels),as_numeric(obpm.levels),as_numeric(obpm.buildinglevels),as_numeric(obpm.rellevels),as_numeric(obpm.relbuildinglevels)) AS levels,
-     greatest(as_numeric(obp.min_level),as_numeric(obpm.min_level),as_numeric(obpm.buildingmin_level),as_numeric(obpm.relmin_level),as_numeric(obpm.relbuildingmin_level)) AS min_level,
-           COALESCE(obpm.role, '') AS role,
-           obpm.member AS member,
-           COALESCE(obpm.building, '') AS building
-        FROM osm_building_polygon AS obp
-        FULL OUTER JOIN 
-        osm_building_polygon_member AS obpm
-        ON (obp.osm_id = obpm.member)
-) AS joined);
-CREATE INDEX IF NOT EXISTS osm_all_buildings_geometry_idx ON all_buildings USING gist(geometry);
-CREATE INDEX IF NOT EXISTS osm_all_buildings_role_idx ON all_buildings(role);
+CREATE INDEX IF NOT EXISTS osm_building_relation_building_idx ON osm_building_relation(building);
+
+CREATE OR REPLACE VIEW osm_all_buildings AS (
+        -- etldoc: osm_building_relation -> layer_building:z14_
+	 SELECT member AS osm_id,geometry,
+                  COALESCE(nullif(as_numeric(height),-1),nullif(as_numeric(buildingheight),-1)) as height,
+                  COALESCE(nullif(as_numeric(min_height),-1),nullif(as_numeric(buildingmin_height),-1)) as min_height,
+                  COALESCE(nullif(as_numeric(levels),-1),nullif(as_numeric(buildinglevels),-1)) as levels,
+                  COALESCE(nullif(as_numeric(min_level),-1),nullif(as_numeric(buildingmin_level),-1)) as min_level
+         FROM
+         osm_building_relation WHERE building = ''
+         UNION ALL
+        -- etldoc: osm_building_polygon -> layer_building:z14_
+         SELECT osm_id,geometry,
+                  COALESCE(nullif(as_numeric(height),-1),nullif(as_numeric(buildingheight),-1)) as height,
+                  COALESCE(nullif(as_numeric(min_height),-1),nullif(as_numeric(buildingmin_height),-1)) as min_height,
+                  COALESCE(nullif(as_numeric(levels),-1),nullif(as_numeric(buildinglevels),-1)) as levels,
+                  COALESCE(nullif(as_numeric(min_level),-1),nullif(as_numeric(buildingmin_level),-1)) as min_level
+         FROM
+         osm_building_polygon obp WHERE EXISTS (SELECT 1 FROM osm_building_multipolygon obm WHERE obp.osm_id = obm.osm_id)
+         UNION ALL
+        -- etldoc: osm_building_polygon -> layer_building:z14_
+         SELECT osm_id,geometry,
+
+                  COALESCE(nullif(as_numeric(height),-1),nullif(as_numeric(buildingheight),-1)) as height,
+                  COALESCE(nullif(as_numeric(min_height),-1),nullif(as_numeric(buildingmin_height),-1)) as min_height,
+                  COALESCE(nullif(as_numeric(levels),-1),nullif(as_numeric(buildinglevels),-1)) as levels,
+                  COALESCE(nullif(as_numeric(min_level),-1),nullif(as_numeric(buildingmin_level),-1)) as min_level
+         FROM
+         osm_building_polygon WHERE osm_id >= 0
+);
 
 CREATE OR REPLACE FUNCTION layer_building(bbox geometry, zoom_level int)
 RETURNS TABLE(geometry geometry, osm_id bigint, render_height int, render_min_height int) AS $$
@@ -48,17 +57,15 @@ RETURNS TABLE(geometry geometry, osm_id bigint, render_height int, render_min_he
         WHERE zoom_level = 13 AND geometry && bbox AND area > 1400
         UNION ALL
         -- etldoc: osm_building_polygon -> layer_building:z14_
-        SELECT
+        SELECT DISTINCT ON (osm_id) 
            osm_id, geometry,
-           ceil(greatest(5, COALESCE(height, levels*3.66,5)))::int AS render_height,
-           floor(greatest(0, COALESCE(min_height, min_level*3.66,0)))::int AS render_min_height FROM
-        all_buildings
-        WHERE zoom_level >= 14 AND role <> 'outline' AND geometry && bbox
+           ceil( COALESCE(height, levels*3.66,5))::int AS render_height,
+           floor(COALESCE(min_height, min_level*3.66,0))::int AS render_min_height FROM
+        osm_all_buildings
+        WHERE zoom_level >= 14 AND geometry && bbox
     ) AS zoom_levels
     ORDER BY render_height ASC, ST_YMin(geometry) DESC;
 $$ LANGUAGE SQL IMMUTABLE;
 
--- drop buildings that overlap obpm, but keep the parts? Or something. Or ignore because of crummy data
--- building:height and height for obp
--- coalesce instead of greatest
+-- not handled: where a building outline covers building parts
 
