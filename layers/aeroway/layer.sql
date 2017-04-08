0f279bea500238e39980b37036880b6c40f954e6
stirringhalo <stirringhalo@users.noreply.github.com>
2017-04-08 16:48:39 -0400

    Fix bug in missing gen3 and switch aeroway to ZRes

--- layers/aeroway/layer.sql
+++ layers/aeroway/layer.sql
@@ -9,7 +9,7 @@ RETURNS TABLE(geometry geometry, class text) AS $$
         -- etldoc:  osm_aeroway_linestring -> layer_aeroway:z13
         -- etldoc:  osm_aeroway_linestring -> layer_aeroway:z14_
         SELECT geometry, aeroway
-        FROM osm_aeroway_linestring WHERE zoom_level >= 11
+        FROM osm_aeroway_linestring_gen3 WHERE zoom_level >= 11
         UNION ALL
         -- etldoc:  osm_aeroway_polygon_gen2 -> layer_aeroway:z12
         SELECT geometry, aeroway
