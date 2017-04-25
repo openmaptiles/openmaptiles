90690d2a39a50e2e24ae8ec94ddb522166bb7499
stirringhalo <stirringhalo@users.noreply.github.com>
2017-04-25 18:03:29 -0400

    Switch to ZRes (#214)

--- quickstart.sh
+++ quickstart.sh
@@ -166,14 +166,14 @@ fi
 echo " "
 echo "-------------------------------------------------------------------------------------"
 echo "====> : Remove old generated source files ( ./build/* ) ( if they exist ) "
-docker run --rm -v $(pwd):/tileset openmaptiles/openmaptiles-tools make clean
+docker run --rm -v $(pwd):/tileset openmaptiles/openmaptiles-tools:PIXEL_GEOUNIT make clean
 
 echo " "
 echo "-------------------------------------------------------------------------------------"
 echo "====> : Code generating from the layer definitions ( ./build/mapping.yaml; ./build/tileset.sql )"
 echo "      : The tool source code: https://github.com/openmaptiles/openmaptiles-tools "
 echo "      : But we generate the tm2source, Imposm mappings and SQL functions from the layer definitions! "
-docker run --rm -v $(pwd):/tileset openmaptiles/openmaptiles-tools make
+docker run --rm -v $(pwd):/tileset openmaptiles/openmaptiles-tools:PIXEL_GEOUNIT make
 
 echo " "
 echo "-------------------------------------------------------------------------------------"
