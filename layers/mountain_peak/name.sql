1193224fe9fa25d202613b6c60fad536dbbaf47f
jirik <jirik@users.noreply.github.com>
2017-05-23 18:07:24 +0200

    Move name functions to import-sql

--- layers/mountain_peak/name.sql
+++ layers/mountain_peak/name.sql
@@ -1,35 +1,3 @@
-CREATE OR REPLACE FUNCTION get_latin_name(tags hstore) RETURNS text AS $$
-    SELECT COALESCE(
-      NULLIF(tags->'name:en', ''),
-      NULLIF(tags->'int_name', ''),
-      CASE
-        WHEN tags->'name' ~ '.*[a-zA-Z].*'
-          THEN tags->'name'
-        ELSE NULL
-      END
-    );
-$$ LANGUAGE SQL IMMUTABLE STRICT;
-
-
-CREATE OR REPLACE FUNCTION get_nonlatin_name(tags hstore) RETURNS text AS $$
-    SELECT
-      CASE
-        WHEN tags->'name' !~ '.*[a-zA-Z].*'
-          THEN tags->'name'
-        ELSE NULL
-      END;
-$$ LANGUAGE SQL IMMUTABLE STRICT;
-
-CREATE OR REPLACE FUNCTION get_name_int(tags hstore) RETURNS text AS $$
-    SELECT
-      COALESCE(
-        NULLIF(tags->'int_name', ''),
-        NULLIF(tags->'name:en', ''),
-        tags->'name'
-      );
-$$ LANGUAGE SQL IMMUTABLE STRICT;
-
-
 DO $$
 BEGIN
   update osm_peak_point SET tags = tags || hstore(ARRAY[
