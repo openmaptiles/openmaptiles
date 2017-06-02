35af53c5026e3e4b50f9dbb5717c9984f7fa7c04
jirik <jirik@users.noreply.github.com>
2017-06-02 12:52:47 +0200

    Use osml10n functions for 5 layers with name tags

--- layers/mountain_peak/name.sql
+++ layers/mountain_peak/name.sql
@@ -1,4 +1,4 @@
 DO $$
 BEGIN
-  update osm_peak_point SET tags = slice_language_tags(tags) || get_basic_names(tags);
+  update osm_peak_point SET tags = slice_language_tags(tags) || get_basic_names(tags, geometry);
 END $$;
