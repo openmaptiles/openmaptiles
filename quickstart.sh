768fcc82050fb6bea0f9b00c11113876f4cf80f3
jirik <jirik@users.noreply.github.com>
2017-06-06 18:19:26 +0200

    Use openmaptiles-tools:0.5 and docker-compose to run it

--- quickstart.sh
+++ quickstart.sh
@@ -166,14 +166,14 @@ fi
 echo " "
 echo "-------------------------------------------------------------------------------------"
 echo "====> : Remove old generated source files ( ./build/* ) ( if they exist ) "
-docker run --rm -v $(pwd):/tileset openmaptiles/openmaptiles-tools make clean
+docker-compose run --rm openmaptiles-tools make clean
 
 echo " "
 echo "-------------------------------------------------------------------------------------"
 echo "====> : Code generating from the layer definitions ( ./build/mapping.yaml; ./build/tileset.sql )"
 echo "      : The tool source code: https://github.com/openmaptiles/openmaptiles-tools "
 echo "      : But we generate the tm2source, Imposm mappings and SQL functions from the layer definitions! "
-docker run --rm -v $(pwd):/tileset openmaptiles/openmaptiles-tools make
+docker-compose run --rm openmaptiles-tools make
 
 echo " "
 echo "-------------------------------------------------------------------------------------"
