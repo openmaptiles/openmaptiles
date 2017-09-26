daeb8c66054c98473c72b3919364e1cdefe2e482
jirik <jirik@users.noreply.github.com>
2017-09-26 14:37:42 +0200

    Show aeroways sooner

--- layers/aeroway/layer.sql
+++ layers/aeroway/layer.sql
@@ -1,15 +1,31 @@
 -- etldoc: layer_aeroway[shape=record fillcolor=lightpink, style="rounded,filled",
--- etldoc:     label="layer_aeroway |<z11> z11|<z12> z12|<z13> z13|<z14_> z14+" ];
+-- etldoc:     label="layer_aeroway |<z10> z10|<z11> z11|<z12> z12|<z13> z13|<z14_> z14+" ];
 
 CREATE OR REPLACE FUNCTION layer_aeroway(bbox geometry, zoom_level int)
 RETURNS TABLE(geometry geometry, class text, ref text) AS $$
     SELECT geometry, aeroway AS class, ref FROM (
-        -- etldoc:  osm_aeroway_linestring -> layer_aeroway:z11
-        -- etldoc:  osm_aeroway_linestring -> layer_aeroway:z12
+        -- etldoc:  osm_aeroway_linestring_gen3 -> layer_aeroway:z10
+        SELECT geometry, aeroway, ref
+        FROM osm_aeroway_linestring_gen3 WHERE zoom_level = 10
+        UNION ALL
+        -- etldoc:  osm_aeroway_linestring_gen2 -> layer_aeroway:z11
+        SELECT geometry, aeroway, ref
+        FROM osm_aeroway_linestring_gen2 WHERE zoom_level = 11
+        UNION ALL
+        -- etldoc:  osm_aeroway_linestring_gen1 -> layer_aeroway:z12
+        SELECT geometry, aeroway, ref
+        FROM osm_aeroway_linestring_gen1 WHERE zoom_level = 12
+        UNION ALL
         -- etldoc:  osm_aeroway_linestring -> layer_aeroway:z13
         -- etldoc:  osm_aeroway_linestring -> layer_aeroway:z14_
         SELECT geometry, aeroway, ref
-        FROM osm_aeroway_linestring WHERE zoom_level >= 11
+        FROM osm_aeroway_linestring WHERE zoom_level >= 13
+        UNION ALL
+
+        -- etldoc:  osm_aeroway_polygon_gen3 -> layer_aeroway:z10
+        -- etldoc:  osm_aeroway_polygon_gen3 -> layer_aeroway:z11
+        SELECT geometry, aeroway, ref
+        FROM osm_aeroway_polygon_gen3 WHERE zoom_level BETWEEN 10 AND 11
         UNION ALL
         -- etldoc:  osm_aeroway_polygon_gen2 -> layer_aeroway:z12
         SELECT geometry, aeroway, ref
