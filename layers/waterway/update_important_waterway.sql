3925c1b194c5981e4a273a25071d668f76f8bf12
jirik <jirik@users.noreply.github.com>
2017-11-16 11:42:48 +0100

    Group important waterways by language tags only

--- layers/waterway/update_important_waterway.sql
+++ layers/waterway/update_important_waterway.sql
@@ -25,10 +25,10 @@ CREATE MATERIALIZED VIEW osm_important_waterway_linestring AS (
     FROM (
         SELECT
             ST_LineMerge(ST_Union(geometry)) AS geometry,
-            name, name_en, name_de, tags
+            name, name_en, name_de, slice_language_tags(tags) AS tags
         FROM osm_waterway_linestring
         WHERE name <> '' AND waterway = 'river'
-        GROUP BY name, name_en, name_de, tags
+        GROUP BY name, name_en, name_de, slice_language_tags(tags)
     ) AS waterway_union
 );
 CREATE INDEX IF NOT EXISTS osm_important_waterway_linestring_geometry_idx ON osm_important_waterway_linestring USING gist(geometry);
