94089e0303179f7ad1349f19d23735c171853a17
bobr11 <bernat.berny@gmail.com>
2018-11-19 23:29:51 +0100

    Issue #420, Added tag natural=volcano as a new layer volcano

---
+++ layers/volcano/layer.sql
@@ -0,0 +1,30 @@
+
+-- etldoc: layer_volcano[shape=record fillcolor=red,
+-- etldoc:     style="rounded,filled", label="layer_volcano | <z7_> z7+" ] ;
+
+CREATE OR REPLACE FUNCTION layer_volcano(bbox geometry, zoom_level integer, pixel_width numeric)
+RETURNS TABLE(osm_id bigint, geometry geometry, name text, name_en text, name_de text, tags hstore, ele int, ele_ft int, "rank" int) AS $$
+   -- etldoc: osm_volcano_point -> layer_volcano:z7_
+   SELECT osm_id, geometry, name, name_en, name_de, tags, ele::int, ele_ft::int, rank::int
+   FROM (
+     SELECT osm_id, geometry, name,
+     COALESCE(NULLIF(name_en, ''), name) AS name_en,
+     COALESCE(NULLIF(name_de, ''), name, name_en) AS name_de,
+     tags,
+     substring(ele from E'^(-?\\d+)(\\D|$)')::int AS ele,
+     round(substring(ele from E'^(-?\\d+)(\\D|$)')::int*3.2808399)::int AS ele_ft,
+       row_number() OVER (
+           PARTITION BY LabelGrid(geometry, 100 * pixel_width)
+           ORDER BY (
+             substring(ele from E'^(-?\\d+)(\\D|$)')::int +
+             (CASE WHEN NULLIF(wikipedia, '') is not null THEN 10000 ELSE 0 END) +
+             (CASE WHEN NULLIF(name, '') is not null THEN 10000 ELSE 0 END)
+           ) DESC
+       )::int AS "rank"
+     FROM osm_volcano_point
+     WHERE geometry && bbox AND ele is not null AND ele ~ E'^-?\\d+'
+   ) AS ranked_volcanoes
+   WHERE zoom_level >= 7 AND (rank <= 5 OR zoom_level >= 14)
+   ORDER BY "rank" ASC;
+
+$$ LANGUAGE SQL IMMUTABLE;
