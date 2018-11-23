5c45714d7380224d2c934dd34f6359816af61898
jirik <jirik@users.noreply.github.com>
2018-11-23 15:27:57 +0100

    Add extra geometry_point && bbox check to park layer

--- layers/park/layer.sql
+++ layers/park/layer.sql
@@ -71,55 +71,55 @@ RETURNS TABLE(osm_id bigint, geometry geometry, class text, name text, name_en t
         -- etldoc: osm_park_polygon_gen8 -> layer_park:z6
         SELECT osm_id, geometry_point, name, name_en, name_de, tags, leisure, boundary, area
         FROM osm_park_polygon_gen8
-        WHERE zoom_level = 6
+        WHERE zoom_level = 6 AND geometry_point && bbox
         UNION ALL
 
         -- etldoc: osm_park_polygon_gen7 -> layer_park:z7
         SELECT osm_id, geometry_point, name, name_en, name_de, tags, leisure, boundary, area
         FROM osm_park_polygon_gen7
-        WHERE zoom_level = 7
+        WHERE zoom_level = 7 AND geometry_point && bbox
         UNION ALL
 
         -- etldoc: osm_park_polygon_gen6 -> layer_park:z8
         SELECT osm_id, geometry_point, name, name_en, name_de, tags, leisure, boundary, area
         FROM osm_park_polygon_gen6
-        WHERE zoom_level = 8
+        WHERE zoom_level = 8 AND geometry_point && bbox
         UNION ALL
 
         -- etldoc: osm_park_polygon_gen5 -> layer_park:z9
         SELECT osm_id, geometry_point, name, name_en, name_de, tags, leisure, boundary, area
         FROM osm_park_polygon_gen5
-        WHERE zoom_level = 9
+        WHERE zoom_level = 9 AND geometry_point && bbox
         UNION ALL
 
         -- etldoc: osm_park_polygon_gen4 -> layer_park:z10
         SELECT osm_id, geometry_point, name, name_en, name_de, tags, leisure, boundary, area
         FROM osm_park_polygon_gen4
-        WHERE zoom_level = 10
+        WHERE zoom_level = 10 AND geometry_point && bbox
         UNION ALL
 
         -- etldoc: osm_park_polygon_gen3 -> layer_park:z11
         SELECT osm_id, geometry_point, name, name_en, name_de, tags, leisure, boundary, area
         FROM osm_park_polygon_gen3
-        WHERE zoom_level = 11
+        WHERE zoom_level = 11 AND geometry_point && bbox
         UNION ALL
 
         -- etldoc: osm_park_polygon_gen2 -> layer_park:z12
         SELECT osm_id, geometry_point, name, name_en, name_de, tags, leisure, boundary, area
         FROM osm_park_polygon_gen2
-        WHERE zoom_level = 12
+        WHERE zoom_level = 12 AND geometry_point && bbox
         UNION ALL
 
         -- etldoc: osm_park_polygon_gen1 -> layer_park:z13
         SELECT osm_id, geometry_point, name, name_en, name_de, tags, leisure, boundary, area
         FROM osm_park_polygon_gen1
-        WHERE zoom_level = 13
+        WHERE zoom_level = 13 AND geometry_point && bbox
         UNION ALL
 
         -- etldoc: osm_park_polygon -> layer_park:z14
         SELECT osm_id, geometry_point, name, name_en, name_de, tags, leisure, boundary, area
         FROM osm_park_polygon
-        WHERE zoom_level >= 14
+        WHERE zoom_level >= 14 AND geometry_point && bbox
     ) AS park_point
     ) AS park_all
 
