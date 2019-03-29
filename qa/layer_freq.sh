8fe9af23279067201450ec1e06dbb33e296ce085
Yuri Astrakhan <yuriastrakhan@gmail.com>
2019-03-29 17:15:42 -0400

    Minor trailing whitespace cleanup (#596)
    Remove extra spaces in a number of files.

--- qa/layer_freq.sh
+++ qa/layer_freq.sh
@@ -11,9 +11,9 @@ echo "# FREQ - $layerid  group by : $classvars "
 for z in {0..15}
 do
 echo " "
-echo "## $layerid z$z  - freq" 
+echo "## $layerid z$z  - freq"
 
-SQL=$(docker run --rm -v $(pwd):/tileset openmaptiles/openmaptiles-tools generate-sqlquery  layers/${layerid}/${layerid}.yaml $z ) 
+SQL=$(docker run --rm -v $(pwd):/tileset openmaptiles/openmaptiles-tools generate-sqlquery  layers/${layerid}/${layerid}.yaml $z )
 
 SQLCODE=$(cat <<-END
 select $classvars , count(*) as _count_ from
@@ -29,7 +29,7 @@ END
 #echo "\`\`\`"
 
 docker-compose run --rm import-osm /usr/src/app/psql.sh -q -P pager=off -P border=2 -P footer=off -P null='(null)' -c "$SQLCODE" \
-   | sed '1d;$d'  | sed '$d' | sed 's/+--/|--/g' | sed 's/--+/--|/g' 
-     
+   | sed '1d;$d'  | sed '$d' | sed 's/+--/|--/g' | sed 's/--+/--|/g'
+
 done
 
