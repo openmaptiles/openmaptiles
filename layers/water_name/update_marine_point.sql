7a3ac079ae2e7b49e502769b11cd530a2073f5ef
zstadler <zeev.stadler@gmail.com>
2019-05-24 11:21:43 +0000

    Add intermittent field to water* layers (#430 PR #585)

--- layers/water_name/update_marine_point.sql
+++ layers/water_name/update_marine_point.sql
@@ -12,7 +12,7 @@ BEGIN
   -- etldoc: osm_marine_point              -> osm_marine_point
 
   WITH important_marine_point AS (
-      SELECT osm.geometry, osm.osm_id, osm.name, osm.name_en, ne.scalerank
+      SELECT osm.geometry, osm.osm_id, osm.name, osm.name_en, ne.scalerank, osm.is_intermittent
       FROM ne_10m_geography_marine_polys AS ne, osm_marine_point AS osm
       WHERE trim(regexp_replace(ne.name, '\\s+', ' ', 'g')) ILIKE osm.name
         OR trim(regexp_replace(ne.name, '\\s+', ' ', 'g')) ILIKE osm.tags->'name:en'
