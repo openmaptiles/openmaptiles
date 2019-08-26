907211dea2e3d1a7c3fc8cf9eef862c71236c099
golubev <gs@lun.ua>
2019-08-26 12:50:17 +0300

    close #657: limit mountain_peak's 'ele' tag to 10000

--- layers/mountain_peak/layer.sql
+++ layers/mountain_peak/layer.sql
@@ -45,7 +45,10 @@ $$
           ) DESC
       )::int AS "rank"
       FROM osm_peak_point
-      WHERE geometry && bbox AND ele is not null AND ele ~ E'^-?\\d+'
+      WHERE geometry && bbox
+        AND ele is not null
+        AND ele ~ E'^-?\\d+'
+        AND substring(ele from E'^(-?\\d+)(\\D|$)')::bigint < 10000
     ) AS ranked_peaks
   WHERE zoom_level >= 7 AND (rank <= 5 OR zoom_level >= 14)
   ORDER BY "rank" ASC;
