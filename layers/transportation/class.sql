8d9a5df48b71da1aabad01c297546ed6a5a2df62
golubev <gs@lun.ua>
2019-10-03 14:10:36 +0300

    layers/transportation/: do a tiny code styling (#321)

--- layers/transportation/class.sql
+++ layers/transportation/class.sql
@@ -17,9 +17,8 @@ CREATE OR REPLACE FUNCTION highway_class(highway TEXT, public_transport TEXT) RE
         WHEN highway IN ('secondary', 'secondary_link') THEN 'secondary'
         WHEN highway IN ('tertiary', 'tertiary_link') THEN 'tertiary'
         WHEN highway IN ('unclassified', 'residential', 'living_street', 'road') THEN 'minor'
-        WHEN highway IN ('service', 'track') THEN highway
         WHEN highway IN ('pedestrian', 'path', 'footway', 'cycleway', 'steps', 'bridleway', 'corridor') OR public_transport IN ('platform') THEN 'path'
-        WHEN highway = 'raceway' THEN 'raceway'
+        WHEN highway IN ('service', 'track', 'raceway') THEN highway
         ELSE NULL
     END;
 $$ LANGUAGE SQL IMMUTABLE;
