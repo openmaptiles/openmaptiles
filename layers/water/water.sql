3ac0f2e316c8d948d15a4e4531bd44c4d3841b24
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2019-12-16 12:18:54 -0500

    improve readability

--- layers/water/water.sql
+++ layers/water/water.sql
@@ -3,7 +3,8 @@ DO $$ BEGIN DROP TABLE IF EXISTS osm_ocean_polygon_gen1 CASCADE; EXCEPTION WHEN 
 -- etldoc: osm_ocean_polygon -> osm_ocean_polygon_gen1
 DROP MATERIALIZED VIEW IF EXISTS osm_ocean_polygon_gen1 CASCADE;
 CREATE MATERIALIZED VIEW osm_ocean_polygon_gen1 AS (
-  SELECT ST_Simplify(geometry, 20) AS geometry FROM osm_ocean_polygon
+  SELECT ST_Simplify(geometry, 20) AS geometry
+  FROM osm_ocean_polygon
 ) /* DELAY_MATERIALIZED_VIEW_CREATION */ ;
 CREATE INDEX IF NOT EXISTS osm_ocean_polygon_gen1_idx ON osm_ocean_polygon_gen1 USING gist (geometry);
 
@@ -13,7 +14,8 @@ DO $$ BEGIN DROP TABLE IF EXISTS osm_ocean_polygon_gen2 CASCADE; EXCEPTION WHEN 
 -- etldoc: osm_ocean_polygon -> osm_ocean_polygon_gen2
 DROP MATERIALIZED VIEW IF EXISTS osm_ocean_polygon_gen2 CASCADE;
 CREATE MATERIALIZED VIEW osm_ocean_polygon_gen2 AS (
-  SELECT ST_Simplify(geometry, 40) AS geometry FROM osm_ocean_polygon
+  SELECT ST_Simplify(geometry, 40) AS geometry
+  FROM osm_ocean_polygon
 ) /* DELAY_MATERIALIZED_VIEW_CREATION */ ;
 CREATE INDEX IF NOT EXISTS osm_ocean_polygon_gen2_idx ON osm_ocean_polygon_gen2 USING gist (geometry);
 
@@ -23,7 +25,8 @@ DO $$ BEGIN DROP TABLE IF EXISTS osm_ocean_polygon_gen3 CASCADE; EXCEPTION WHEN 
 -- etldoc: osm_ocean_polygon -> osm_ocean_polygon_gen3
 DROP MATERIALIZED VIEW IF EXISTS osm_ocean_polygon_gen3 CASCADE;
 CREATE MATERIALIZED VIEW osm_ocean_polygon_gen3 AS (
-  SELECT ST_Simplify(geometry, 80) AS geometry FROM osm_ocean_polygon
+  SELECT ST_Simplify(geometry, 80) AS geometry
+  FROM osm_ocean_polygon
 ) /* DELAY_MATERIALIZED_VIEW_CREATION */ ;
 CREATE INDEX IF NOT EXISTS osm_ocean_polygon_gen3_idx ON osm_ocean_polygon_gen3 USING gist (geometry);
 
@@ -33,7 +36,8 @@ DO $$ BEGIN DROP TABLE IF EXISTS osm_ocean_polygon_gen4 CASCADE; EXCEPTION WHEN 
 -- etldoc: osm_ocean_polygon -> osm_ocean_polygon_gen4
 DROP MATERIALIZED VIEW IF EXISTS osm_ocean_polygon_gen4 CASCADE;
 CREATE MATERIALIZED VIEW osm_ocean_polygon_gen4 AS (
-  SELECT ST_Simplify(geometry, 160) AS geometry FROM osm_ocean_polygon
+  SELECT ST_Simplify(geometry, 160) AS geometry
+  FROM osm_ocean_polygon
 ) /* DELAY_MATERIALIZED_VIEW_CREATION */ ;
 CREATE INDEX IF NOT EXISTS osm_ocean_polygon_gen4_idx ON osm_ocean_polygon_gen4 USING gist (geometry);
 
