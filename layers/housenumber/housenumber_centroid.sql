8ba2e18dea882601432f768fc17b7de051ad47bb
Eva Jelinkova <eva.jelinkova@maptiler.com>
2020-11-04 21:07:09 +0100

    Revert "Housenumbers - diff update, multipolygons (#990)" (#1032)
    This reverts commit f7d7a0ced3279597dba49f530b8668f934757988.

--- layers/housenumber/housenumber_centroid.sql
+++ layers/housenumber/housenumber_centroid.sql
@@ -18,11 +18,10 @@ $$
       SELECT pt.osm_id
       FROM osm_housenumber_point pt
       INNER JOIN osm_housenumber_point poly
-      ON (ST_GeometryType(poly.geometry) <> 'ST_Point'
+      ON (ST_GeometryType(poly.geometry) = 'ST_Polygon'
           AND ST_GeometryType(pt.geometry) = 'ST_Point'
           AND pt.geometry && poly.geometry
           AND pt.housenumber = poly.housenumber
-          AND (full_update OR pt.osm_id IN (SELECT osm_id FROM housenumber.osm_ids))
       )
     );
 
