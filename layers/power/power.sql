1bc93cee6443f723b145c804cab0dd5c435aee30
ache051 <ache051@aucklanduni.ac.nz>
2021-09-18 15:33:20 +1200

    Update power.sql
    Clean-up old stuff.

--- layers/power/power.sql
+++ layers/power/power.sql
@@ -1,27 +1,3 @@
-
--- CREATE OR REPLACE FUNCTION layer_power(bbox geometry, zoom_level int)
--- RETURNS TABLE(geometry geometry, class text, source text, method text, name text) AS $$
--- SELECT geometry, class, source, method, name
--- FROM(
-    -- SELECT geometry, class, source, method, name
-    -- FROM osm_power_point
-    -- WHERE zoom_level >= 10 AND geometry && bbox
-    -- UNION ALL
-    
-    -- SELECT geometry, class, source, method, name
-    -- FROM osm_power_linestring
-    -- WHERE zoom_level >= 10 AND geometry && bbox
-    -- UNION ALL
-    
-    -- SELECT geometry, class, source, method, name
-    -- FROM osm_power_polygon
-    -- WHERE zoom_level >= 10 AND geometry && bbox 
-    -- ) AS zoom_levels
--- WHERE geometry && bbox;
--- $$ LANGUAGE SQL STABLE
-                -- -- STRICT
-                -- PARALLEL SAFE;
-
 CREATE OR REPLACE FUNCTION layer_power(bbox geometry, zoom_level int)
 RETURNS TABLE(geometry geometry, name text, class text) AS $$
 SELECT geometry, name, class
