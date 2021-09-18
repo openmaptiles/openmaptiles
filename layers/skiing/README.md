7bbca699c5e1e9ac2c3859f8dc0219ac8b7806da
ache051 <ache051@aucklanduni.ac.nz>
2021-09-18 15:28:13 +1200

    Remove skiiing layer to avoid confusion

--- layers/skiing/README.md
+++
@@ -1,9 +0,0 @@
-## skiing
-
-### Docs
-
-### Mapping Diagram
-![Mapping diagram for transportation](mapping_diagram.png?raw=true)
-
-### ETL diagram
-![ETL diagram for transportation](etl_diagram.png?raw=true)
--- layers/skiing/skiing.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE OR REPLACE FUNCTION layer_skiing(bbox geometry, zoom_level int)
-RETURNS TABLE(geometry geometry, class text, name text) AS $$
-    SELECT geometry, class, name
-    FROM osm_skiing_linestring
-    WHERE zoom_level >= 10 AND geometry && bbox;
-$$ LANGUAGE SQL IMMUTABLE;
