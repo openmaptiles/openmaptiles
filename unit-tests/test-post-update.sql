f69aed804f8d1486a221da5b2a14097ce44bbce9
Brian Sperlongano <zelonewolf@gmail.com>
2021-10-02 23:44:17 +0100

    Fix update unit tests

--- unit-tests/test-post-update.sql
+++ unit-tests/test-post-update.sql
@@ -6,14 +6,17 @@ DECLARE
   cnt integer;
 
 BEGIN
-  RAISE 'fail' USING ERRCODE='0Z000';
 
   -- Test 100
+  SELECT COUNT(*) INTO cnt FROM osm_park_polygon_gen_z5 WHERE boundary='national_park';
+  IF cnt <> 1 THEN
+    INSERT INTO omt_test_failures VALUES(100, 'update', 'osm_park_polygon_gen_z5 national_park expected 1, got ' || cnt);
+  END IF;
 
---  SELECT COUNT(*) INTO cnt FROM osm_park_polygon_gen_z5 WHERE boundary='national_park';
---  IF cnt <> 1 THEN
---    RAISE 'Test 100 Failed: osm_park_polygon_gen_z5 national_park expected 1, got %', cnt USING ERRCODE = '0Z000';
---  END IF;
+  SELECT COUNT(*) INTO cnt FROM omt_test_failures;
+  IF cnt > 0 THEN
+    RAISE '% unit test(s) Failed.  Details can be found in table omt_test_failures.', cnt USING ERRCODE = '0Z000';
+  END IF;
 
 END;
 
