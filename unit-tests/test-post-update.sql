f8ea4aa30a6dad202e35f57212ed2d13de13a18a
Brian Sperlongano <zelonewolf@gmail.com>
2021-10-13 10:04:12 -0400

    Separate test and check functionality

--- unit-tests/test-post-update.sql
+++ unit-tests/test-post-update.sql
@@ -1,12 +1,13 @@
 -- Checks to ensure that test data was imported correctly
-CREATE OR REPLACE PROCEDURE test_update()
-LANGUAGE plpgsql
-AS $$
+DO $$
 
 DECLARE
   cnt integer;
 
 BEGIN
+
+  -- Clear prior results
+  DELETE FROM omt_test_failures WHERE test_type='update';
 
   -- Test 100
   SELECT COUNT(*) INTO cnt FROM osm_park_polygon_gen_z5 WHERE boundary='national_park';
@@ -14,18 +15,20 @@ BEGIN
     INSERT INTO omt_test_failures VALUES(100, 'update', 'osm_park_polygon_gen_z5 national_park expected 1, got ' || cnt);
   END IF;
 
-  SELECT COUNT(*) INTO cnt FROM omt_test_failures;
-  IF cnt > 0 THEN
-    RAISE '% unit test(s) failed on updates.  Details can be found in table omt_test_failures.', cnt USING ERRCODE = '0Z000';
-  END IF;
-
 END;
 
 $$;
 
+
 DO $$
+
+DECLARE
+  cnt integer;
 BEGIN
--- Run all tests
-  PERFORM test_update();
+  SELECT COUNT(*) INTO cnt FROM omt_test_failures;
+  IF cnt > 0 THEN
+    RAISE '% unit test(s) failed on updates.  Details can be found in table omt_test_failures.', cnt USING ERRCODE = '0Z000';
+  END IF;
 END;
+
 $$;
