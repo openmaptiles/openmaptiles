ece9058ce1b6de5c96f34e1eafb933304c76d265
Brian Sperlongano <zelonewolf@gmail.com>
2021-10-21 22:34:43 -0400

    Decrease generalization

--- layers/park/park_gen.sql
+++ layers/park/park_gen.sql
@@ -1,37 +1,37 @@
 UPDATE osm_park_polygon_geometry
-   SET geom_z13 = ST_Simplify(geometry, ZRes(12))
- WHERE area>power(ZRes(12),2);
+   SET geom_z13 = ST_Simplify(geometry, ZRes(13))
+ WHERE area>power(ZRes(13),2);
 
 UPDATE osm_park_polygon_geometry
-   SET geom_z12 = ST_Simplify(geom_z13, ZRes(11))
+   SET geom_z12 = ST_Simplify(geom_z13, ZRes(12))
  WHERE geom_z13 IS NOT NULL;
 
 UPDATE osm_park_polygon_geometry
-   SET geom_z11 = ST_Simplify(geom_z12, ZRes(10))
+   SET geom_z11 = ST_Simplify(geom_z12, ZRes(11))
  WHERE geom_z12 IS NOT NULL;
 
 UPDATE osm_park_polygon_geometry
-   SET geom_z10 = ST_Simplify(geom_z11, ZRes(9))
+   SET geom_z10 = ST_Simplify(geom_z11, ZRes(10))
  WHERE geom_z11 IS NOT NULL;
 
 UPDATE osm_park_polygon_geometry
-   SET geom_z9 = ST_Simplify(geom_z10, ZRes(8))
+   SET geom_z9 = ST_Simplify(geom_z10, ZRes(9))
  WHERE geom_z10 IS NOT NULL;
 
 UPDATE osm_park_polygon_geometry
-   SET geom_z8 = ST_Simplify(geom_z9, ZRes(7))
+   SET geom_z8 = ST_Simplify(geom_z9, ZRes(8))
  WHERE geom_z9 IS NOT NULL;
 
 UPDATE osm_park_polygon_geometry
-   SET geom_z7 = ST_Simplify(geom_z8, ZRes(6))
+   SET geom_z7 = ST_Simplify(geom_z8, ZRes(7))
  WHERE geom_z8 IS NOT NULL;
 
 UPDATE osm_park_polygon_geometry
-   SET geom_z6 = ST_Simplify(geom_z7, ZRes(5))
+   SET geom_z6 = ST_Simplify(geom_z7, ZRes(6))
  WHERE geom_z7 IS NOT NULL;
 
 UPDATE osm_park_polygon_geometry
-   SET geom_z5 = ST_Simplify(geom_z6, ZRes(4))
+   SET geom_z5 = ST_Simplify(geom_z6, ZRes(5))
  WHERE geom_z6 IS NOT NULL;
 
 DROP MATERIALIZED VIEW IF EXISTS osm_park_polygon_dissolve_z4 CASCADE;
