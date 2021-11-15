93d57a8d039cb91219b6d6bbf01c3506830a5529
Eva Jelinkova <eva.jelinkova@maptiler.com>
2021-11-15 16:55:35 +0100

    adding customary_ft for peaks in US (#1298)
    Add tagging that will mark unit that is customary in the country. US peaks will include the tag `customary_ft=1`.

--- layers/mountain_peak/mountain_peak.sql
+++ layers/mountain_peak/mountain_peak.sql
@@ -1,3 +1,22 @@
+-- etldoc: osm_peak_point -> peak_point
+-- etldoc: ne_10m_admin_0_countries -> peak_point
+CREATE OR REPLACE VIEW peak_point AS
+(
+SELECT pp.osm_id,
+       pp.geometry,
+       pp.name,
+       pp.name_en,
+       pp.name_de,
+       pp.tags,
+       pp.ele,
+       ne.iso_a2,
+       pp.wikipedia
+FROM osm_peak_point pp, ne_10m_admin_0_countries ne
+WHERE ST_Intersects(pp.geometry, ne.geometry)
+    );
+
+
+
 -- etldoc: layer_mountain_peak[shape=record fillcolor=lightpink,
 -- etldoc:     style="rounded,filled", label="layer_mountain_peak | <z7_> z7+ | <z13_> z13+" ] ;
 
@@ -6,21 +25,22 @@ CREATE OR REPLACE FUNCTION layer_mountain_peak(bbox geometry,
                                                pixel_width numeric)
     RETURNS TABLE
             (
-                osm_id   bigint,
-                geometry geometry,
-                name     text,
-                name_en  text,
-                name_de  text,
-                class    text,
-                tags     hstore,
-                ele      int,
-                ele_ft   int,
-                "rank"   int
+                osm_id          bigint,
+                geometry        geometry,
+                name            text,
+                name_en         text,
+                name_de         text,
+                class           text,
+                tags            hstore,
+                ele             int,
+                ele_ft          int,
+                customary_ft    int,
+                "rank"          int
             )
 AS
 $$
 SELECT
-    -- etldoc: osm_peak_point -> layer_mountain_peak:z7_
+    -- etldoc: peak_point -> layer_mountain_peak:z7_
     osm_id,
     geometry,
     name,
@@ -30,6 +50,7 @@ SELECT
     tags,
     ele::int,
     ele_ft::int,
+    customary_ft,
     rank::int
 FROM (
          SELECT osm_id,
@@ -40,6 +61,7 @@ FROM (
                 tags,
                 substring(ele FROM E'^(-?\\d+)(\\D|$)')::int AS ele,
                 round(substring(ele FROM E'^(-?\\d+)(\\D|$)')::int * 3.2808399)::int AS ele_ft,
+                CASE WHEN iso_a2 = 'US' THEN 1 END AS customary_ft,
                 row_number() OVER (
                     PARTITION BY LabelGrid(geometry, 100 * pixel_width)
                     ORDER BY (
@@ -48,7 +70,7 @@ FROM (
                             (CASE WHEN name <> '' THEN 10000 ELSE 0 END)
                         ) DESC
                     )::int AS "rank"
-         FROM osm_peak_point
+         FROM peak_point
          WHERE geometry && bbox
            AND ele IS NOT NULL
            AND ele ~ E'^-?\\d{1,4}(\\D|$)'
@@ -69,6 +91,7 @@ SELECT
     tags,
     NULL AS ele,
     NULL AS ele_ft,
+    NULL AS customary_ft,
     rank::int
 FROM (
          SELECT osm_id,
