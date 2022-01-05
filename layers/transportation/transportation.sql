openmaptiles/openmaptiles

commit 75d8c80228c31898c7972cd94a4ecdfb6f173212

Selective rendering of tracks and paths at z12-13 (#1190)
Closes #271

This PR adds track and path rendering at lower zooms than currently provided, and also achieves near-parity with openstreetmap-carto on track and path rendering. A previously-abandoned attempt, with significant discussion, was #1169.

layers/transportation/transportation.sql
@@ -329,9 +329,12 @@ FROM (
          -- etldoc: osm_highway_linestring  ->  layer_transportation:z12
          -- etldoc: osm_highway_linestring  ->  layer_transportation:z13
          -- etldoc: osm_highway_linestring  ->  layer_transportation:z14_
-         SELECT osm_id,
-                geometry,
-                highway,
+         -- etldoc: osm_transportation_name_network  ->  layer_transportation:z12
+         -- etldoc: osm_transportation_name_network  ->  layer_transportation:z13
+         -- etldoc: osm_transportation_name_network  ->  layer_transportation:z14_
+         SELECT hl.osm_id,
+                hl.geometry,
+                hl.highway,
                 construction,
                 network,
                 NULL AS railway,
@@ -347,25 +350,34 @@ FROM (
                 is_ramp,
                 is_oneway,
                 man_made,
-                layer,
-                CASE WHEN highway IN ('footway', 'steps') THEN "level" END AS "level",
-                CASE WHEN highway IN ('footway', 'steps') THEN indoor END AS indoor,
+                hl.layer,
+                CASE WHEN hl.highway IN ('footway', 'steps') THEN hl.level END AS level,
+                CASE WHEN hl.highway IN ('footway', 'steps') THEN hl.indoor END AS indoor,
                 bicycle,
                 foot,
                 horse,
                 mtb_scale,
                 surface_value(surface) AS "surface",
-                z_order
-         FROM osm_highway_linestring
+                hl.z_order
+         FROM osm_highway_linestring hl
+         LEFT OUTER JOIN osm_transportation_name_network n ON hl.osm_id = n.osm_id
          WHERE NOT is_area
            AND
-               CASE WHEN zoom_level = 12 THEN transportation_filter_z12(highway, construction)
+               CASE WHEN zoom_level = 12 THEN
+                         CASE WHEN transportation_filter_z12(hl.highway, hl.construction) THEN TRUE
+                              WHEN n.route_rank = 1 THEN TRUE
+                         END
                     WHEN zoom_level = 13 THEN
-                         CASE WHEN man_made='pier' THEN NOT ST_IsClosed(geometry)
-                              ELSE transportation_filter_z13(highway, public_transport, construction, service)
+                         CASE WHEN man_made='pier' THEN NOT ST_IsClosed(hl.geometry)
+                              WHEN hl.highway = 'path' THEN (
+                                                        hl.name <> ''
+                                                     OR n.route_rank BETWEEN 1 AND 2
+                                                     OR hl.sac_scale <> ''
+                                                   )
+                              ELSE transportation_filter_z13(hl.highway, public_transport, hl.construction, service)
                          END
                     WHEN zoom_level >= 14 THEN
-                         CASE WHEN man_made='pier' THEN NOT ST_IsClosed(geometry)
+                         CASE WHEN man_made='pier' THEN NOT ST_IsClosed(hl.geometry)
                               ELSE TRUE
                          END
                END

layers/transportation/mapping.yaml
@@ -151,6 +151,10 @@ mtb_scale_field: &mtb_scale
   key: mtb:scale
   name: mtb_scale
   type: string
+sac_scale_field: &sac_scale
+  key: sac_scale
+  name: sac_scale
+  type: string
 surface_field: &surface
   key: surface
   name: surface
@@ -200,6 +204,7 @@ tables:
     - *foot
     - *horse
     - *mtb_scale
+    - *sac_scale
     - *surface
     mapping:
       highway:
@@ -425,6 +430,13 @@ tables:
     - *ref
     - *network
     - *name
+    - name: osmc_symbol
+      key: osmc:symbol
+      type: string
+    - name: colour
+      key: colour
+      type: string
     mapping:
       route:
       - road
+      - hiking

layers/transportation/update_route_member.sql
@@ -60,17 +60,22 @@ BEGIN
              JOIN transportation_name.network_changes AS c ON
         r.osm_id = c.osm_id;
 
-    INSERT INTO osm_route_member (id, osm_id, network_type, concurrency_index)
+    INSERT INTO osm_route_member (id, osm_id, network_type, concurrency_index, rank)
     SELECT
       id,
       osm_id,
       osm_route_member_network_type(network) AS network_type,
-      DENSE_RANK() over (PARTITION BY member ORDER BY network_type, network, LENGTH(ref), ref) AS concurrency_index
+      DENSE_RANK() over (PARTITION BY member ORDER BY network_type, network, LENGTH(ref), ref) AS concurrency_index,
+      CASE
+           WHEN network IN ('iwn', 'nwn', 'rwn') THEN 1
+           WHEN network = 'lwn' THEN 2
+           WHEN osmc_symbol || colour <> '' THEN 2
+      END AS rank
     FROM osm_route_member rm
     WHERE rm.member IN
       (SELECT DISTINCT osm_id FROM transportation_name.network_changes)
-    ON CONFLICT (id, osm_id) DO UPDATE SET concurrency_index = EXCLUDED.concurrency_index;
-
+    ON CONFLICT (id, osm_id) DO UPDATE SET concurrency_index = EXCLUDED.concurrency_index,
+                                           rank = EXCLUDED.rank;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -84,16 +89,22 @@ CREATE INDEX IF NOT EXISTS osm_route_member_network_type_idx ON osm_route_member
 CREATE INDEX IF NOT EXISTS osm_highway_linestring_osm_id_idx ON osm_highway_linestring ("osm_id");
 CREATE INDEX IF NOT EXISTS osm_highway_linestring_gen_z11_osm_id_idx ON osm_highway_linestring_gen_z11 ("osm_id");
 
-ALTER TABLE osm_route_member ADD COLUMN IF NOT EXISTS concurrency_index int;
+ALTER TABLE osm_route_member ADD COLUMN IF NOT EXISTS concurrency_index int,
+                             ADD COLUMN IF NOT EXISTS rank int;
 
 -- One-time load of concurrency indexes; updates occur via trigger
-INSERT INTO osm_route_member (id, osm_id, concurrency_index)
+INSERT INTO osm_route_member (id, osm_id, concurrency_index, rank)
   SELECT
     id,
     osm_id,
-    DENSE_RANK() over (PARTITION BY member ORDER BY network_type, network, LENGTH(ref), ref) AS concurrency_index
+    DENSE_RANK() over (PARTITION BY member ORDER BY network_type, network, LENGTH(ref), ref) AS concurrency_index,
+    CASE
+         WHEN network IN ('iwn', 'nwn', 'rwn') THEN 1
+         WHEN network = 'lwn' THEN 2
+         WHEN osmc_symbol || colour <> '' THEN 2
+    END AS rank
   FROM osm_route_member
-  ON CONFLICT (id, osm_id) DO UPDATE SET concurrency_index = EXCLUDED.concurrency_index;
+  ON CONFLICT (id, osm_id) DO UPDATE SET concurrency_index = EXCLUDED.concurrency_index, rank = EXCLUDED.rank;
 
 UPDATE osm_highway_linestring hl
   SET network = rm.network_type

layers/transportation/update_transportation_merge.sql
@@ -6,6 +6,70 @@ DROP TRIGGER IF EXISTS trigger_refresh ON transportation.updates;
 -- to allow for nice label rendering
 -- Because this works well for roads that do not have relations as well
 
+-- etldoc: osm_highway_linestring ->  osm_transportation_name_network
+-- etldoc: osm_route_member ->  osm_transportation_name_network
+CREATE TABLE IF NOT EXISTS osm_transportation_name_network AS
+SELECT
+    geometry,
+    osm_id,
+    name,
+    name_en,
+    name_de,
+    tags,
+    ref,
+    highway,
+    subclass,
+    brunnel,
+    "level",
+    sac_scale,
+    layer,
+    indoor,
+    network_type,
+    route_1, route_2, route_3, route_4, route_5, route_6,
+    z_order,
+    route_rank
+FROM (
+    SELECT DISTINCT ON (hl.osm_id)
+        hl.geometry,
+        hl.osm_id,
+        CASE WHEN length(hl.name) > 15 THEN osml10n_street_abbrev_all(hl.name) ELSE NULLIF(hl.name, '') END AS "name",
+        CASE WHEN length(hl.name_en) > 15 THEN osml10n_street_abbrev_en(hl.name_en) ELSE NULLIF(hl.name_en, '') END AS "name_en",
+        CASE WHEN length(hl.name_de) > 15 THEN osml10n_street_abbrev_de(hl.name_de) ELSE NULLIF(hl.name_de, '') END AS "name_de",
+        slice_language_tags(hl.tags) AS tags,
+        rm1.network_type,
+        CASE
+            WHEN rm1.network_type IS NOT NULL AND rm1.ref::text <> ''
+                THEN rm1.ref::text
+            ELSE NULLIF(hl.ref, '')
+            END AS ref,
+        hl.highway,
+        NULLIF(hl.construction, '') AS subclass,
+        brunnel(hl.is_bridge, hl.is_tunnel, hl.is_ford) AS brunnel,
+        sac_scale,
+        CASE WHEN highway IN ('footway', 'steps') THEN layer END AS layer,
+        CASE WHEN highway IN ('footway', 'steps') THEN level END AS level,
+        CASE WHEN highway IN ('footway', 'steps') THEN indoor END AS indoor,
+        NULLIF(rm1.network, '') || '=' || COALESCE(rm1.ref, '') AS route_1,
+        NULLIF(rm2.network, '') || '=' || COALESCE(rm2.ref, '') AS route_2,
+        NULLIF(rm3.network, '') || '=' || COALESCE(rm3.ref, '') AS route_3,
+        NULLIF(rm4.network, '') || '=' || COALESCE(rm4.ref, '') AS route_4,
+        NULLIF(rm5.network, '') || '=' || COALESCE(rm5.ref, '') AS route_5,
+        NULLIF(rm6.network, '') || '=' || COALESCE(rm6.ref, '') AS route_6,
+        hl.z_order,
+        LEAST(rm1.rank, rm2.rank, rm3.rank, rm4.rank, rm5.rank, rm6.rank) AS route_rank
+    FROM osm_highway_linestring hl
+            LEFT OUTER JOIN osm_route_member rm1 ON rm1.member = hl.osm_id AND rm1.concurrency_index=1
+            LEFT OUTER JOIN osm_route_member rm2 ON rm2.member = hl.osm_id AND rm2.concurrency_index=2
+            LEFT OUTER JOIN osm_route_member rm3 ON rm3.member = hl.osm_id AND rm3.concurrency_index=3
+            LEFT OUTER JOIN osm_route_member rm4 ON rm4.member = hl.osm_id AND rm4.concurrency_index=4
+            LEFT OUTER JOIN osm_route_member rm5 ON rm5.member = hl.osm_id AND rm5.concurrency_index=5
+            LEFT OUTER JOIN osm_route_member rm6 ON rm6.member = hl.osm_id AND rm6.concurrency_index=6
+    WHERE (hl.name <> '' OR hl.ref <> '' OR rm1.ref <> '' OR rm1.network <> '')
+      AND hl.highway <> ''
+) AS t;
+CREATE UNIQUE INDEX IF NOT EXISTS osm_transportation_name_network_osm_id_idx ON osm_transportation_name_network (osm_id);
+CREATE INDEX IF NOT EXISTS osm_transportation_name_network_name_ref_idx ON osm_transportation_name_network (coalesce(name, ''), coalesce(ref, ''));
+CREATE INDEX IF NOT EXISTS osm_transportation_name_network_geometry_idx ON osm_transportation_name_network USING gist (geometry);
 
 -- Improve performance of the sql in transportation/update_route_member.sql
 CREATE INDEX IF NOT EXISTS osm_highway_linestring_highway_partial_idx
@@ -29,6 +93,7 @@ SELECT (ST_Dump(ST_LineMerge(ST_Collect(geometry)))).geom AS geometry,
        foot,
        horse,
        mtb_scale,
+       sac_scale,
        CASE
            WHEN access IN ('private', 'no') THEN 'no'
            ELSE NULL::text END AS access,
@@ -36,7 +101,7 @@ SELECT (ST_Dump(ST_LineMerge(ST_Collect(geometry)))).geom AS geometry,
        layer
 FROM osm_highway_linestring_gen_z11
 -- mapping.yaml pre-filter: motorway/trunk/primary/secondary/tertiary, with _link variants, construction, ST_IsValid()
-GROUP BY highway, network, construction, is_bridge, is_tunnel, is_ford, bicycle, foot, horse, mtb_scale, access, toll, layer
+GROUP BY highway, network, construction, is_bridge, is_tunnel, is_ford, bicycle, foot, horse, mtb_scale, sac_scale, access, toll, layer
     ) /* DELAY_MATERIALIZED_VIEW_CREATION */;
 CREATE INDEX IF NOT EXISTS osm_transportation_merge_linestring_gen_z11_geometry_idx
     ON osm_transportation_merge_linestring_gen_z11 USING gist (geometry);
@@ -57,6 +122,7 @@ SELECT ST_Simplify(geometry, ZRes(12)) AS geometry,
        foot,
        horse,
        mtb_scale,
+       sac_scale,
        access,
        toll,
        layer
@@ -83,6 +149,7 @@ SELECT ST_Simplify(geometry, ZRes(11)) AS geometry,
        foot,
        horse,
        mtb_scale,
+       sac_scale,
        access,
        toll,
        layer

commit 2e04f831664eb303e98354bb9dba0ed2511dc998

Fix paths and tracks in transportation layer z12 and z13 (#1334)
This PR fixes a bug that causes that `track` lines disappear at z13. This bug was introduced in https://github.com/openmaptiles/openmaptiles/pull/1190, which adds rendering of paths and tracks at z12 and z13.

Before this PR:
z12: lines with `route_rank = 1` are added (no matter what `highway`).
z13: lines with `route_rank BETWEEN 1 AND 2` and `highway = 'path'` are added.
-> tracks with `route_rank=1` are added at z12 but not at z13.

After this PR
z12: lines with `route_rank = 1` and `highway IN ('path', 'track')` are added.
z13: lines with `route_rank BETWEEN 1 AND 2` and `highway IN ('path', 'track')` are added .
-> only tracks and paths are added at z12 and z13 (which was IMHO the goal of https://github.com/openmaptiles/openmaptiles/pull/1190)

* Add only the most important paths and tracks (route_rank=1) to z12 and more important ones (route_rank between 1-2 or sac_scale or has name) to z13.

layers/transportation/transportation.sql
@@ -376,15 +376,14 @@ FROM (
            AND
                CASE WHEN zoom_level = 12 THEN
                          CASE WHEN transportation_filter_z12(hl.highway, hl.construction) THEN TRUE
-                              WHEN n.route_rank = 1 THEN TRUE
+                              WHEN hl.highway IN ('track', 'path') THEN n.route_rank = 1
                          END
                     WHEN zoom_level = 13 THEN
                          CASE WHEN man_made='pier' THEN NOT ST_IsClosed(hl.geometry)
-                              WHEN hl.highway = 'path' THEN (
-                                                        hl.name <> ''
-                                                     OR n.route_rank BETWEEN 1 AND 2
-                                                     OR hl.sac_scale <> ''
-                                                   )
+                              WHEN hl.highway IN ('track', 'path') THEN (hl.name <> ''
+                                                                   OR n.route_rank BETWEEN 1 AND 2
+                                                                   OR hl.sac_scale <> ''
+                                                                   )
                               ELSE transportation_filter_z13(hl.highway, public_transport, hl.construction, service)
                          END
                     WHEN zoom_level >= 14 THEN