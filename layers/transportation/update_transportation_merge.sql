51d2d8979067d8e5d88561a24c89ebb45ef9661c
Christopher Lorenz <britiger@gmx.net>
2022-04-14 22:31:17 +0200

    Add segregated

--- layers/transportation/update_transportation_merge.sql
+++ layers/transportation/update_transportation_merge.sql
@@ -85,6 +85,7 @@ SELECT (ST_Dump(ST_LineMerge(ST_Collect(geometry)))).geom AS geometry,
        expressway,
        is_motorroad,
        min(z_order) as z_order,
+       segregated,
        bicycle,
        is_bicycle_road,
        bicycle_forward,
@@ -114,7 +115,7 @@ SELECT (ST_Dump(ST_LineMerge(ST_Collect(geometry)))).geom AS geometry,
        layer
 FROM osm_highway_linestring_gen_z11
 -- mapping.yaml pre-filter: motorway/trunk/primary/secondary/tertiary, with _link variants, construction, ST_IsValid()
-GROUP BY highway, network, construction, is_bridge, is_tunnel, is_ford, expressway, is_motorroad, bicycle, is_bicycle_road, bicycle_forward, bicycle_backward, cycleway, cycleway_both, cycleway_left, cycleway_right, sidewalk_bicycle, sidewalk_both_bicycle, sidewalk_left_bicycle, sidewalk_right_bicycle, maxspeed, maxspeed_forward, maxspeed_backward, traffic_sign, traffic_sign_forward, traffic_sign_backward, foot, horse, mtb_scale, sac_scale, access, toll, layer
+GROUP BY highway, network, construction, is_bridge, is_tunnel, is_ford, expressway, is_motorroad, segregated, bicycle, is_bicycle_road, bicycle_forward, bicycle_backward, cycleway, cycleway_both, cycleway_left, cycleway_right, sidewalk_bicycle, sidewalk_both_bicycle, sidewalk_left_bicycle, sidewalk_right_bicycle, maxspeed, maxspeed_forward, maxspeed_backward, traffic_sign, traffic_sign_forward, traffic_sign_backward, foot, horse, mtb_scale, sac_scale, access, toll, layer
     ) /* DELAY_MATERIALIZED_VIEW_CREATION */;
 CREATE INDEX IF NOT EXISTS osm_transportation_merge_linestring_gen_z11_geometry_idx
     ON osm_transportation_merge_linestring_gen_z11 USING gist (geometry);
@@ -133,6 +134,7 @@ SELECT ST_Simplify(geometry, ZRes(12)) AS geometry,
        expressway,
        is_motorroad,
        z_order,
+       segregated,
        bicycle,
        is_bicycle_road,
        bicycle_forward,
@@ -179,6 +181,7 @@ SELECT ST_Simplify(geometry, ZRes(11)) AS geometry,
        expressway,
        is_motorroad,
        z_order,
+       segregated,
        bicycle,
        is_bicycle_road,
        bicycle_forward,
