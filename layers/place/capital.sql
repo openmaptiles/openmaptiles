2c57458b93ce828340fe3786bd96c78ef6cfc6f3
Brian Sperlongano <zelonewolf@gmail.com>
2022-06-12 22:04:24 -0400

    Formatting

--- layers/place/capital.sql
+++ layers/place/capital.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION normalize_capital_level(capital text)
     RETURNS int AS
 $$
 SELECT CASE
-           WHEN capital='yes' THEN 2
+           WHEN capital = 'yes' THEN 2
            WHEN capital IN ('2', '4', '6') THEN capital::int
            END;
 $$ LANGUAGE SQL IMMUTABLE
