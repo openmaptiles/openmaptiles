11d7b634c55d4cf11c4025517051448adb1e93b2
Brian Sperlongano <zelonewolf@gmail.com>
2023-07-07 18:28:26 -0400

    Convert plpgsql to IMMUTABLE SQL

--- layers/housenumber/housenumber_display.sql
+++ layers/housenumber/housenumber_display.sql
@@ -1,37 +1,20 @@
 CREATE OR REPLACE FUNCTION display_housenumber_nonnumeric(raw_housenumber text)
 RETURNS text AS $$
-DECLARE
-  arr text[];
-BEGIN
-  -- Convert the input string into an array of values
-  arr := string_to_array(raw_housenumber, ';')::text[];
-
-  -- Return the first and last value in the array
-  RETURN arr[1] || '–' || arr[array_length(arr, 1)];
-END;
-$$ LANGUAGE plpgsql;
+  -- Find the position of the semicolon in the input string
+  -- and extract the first and last value
+  SELECT substring(raw_housenumber from 1 for position(';' in raw_housenumber) - 1)
+         || '–'
+         || substring(raw_housenumber from position(';' in raw_housenumber) + 1);
+$$ LANGUAGE SQL IMMUTABLE;
 
 
 CREATE OR REPLACE FUNCTION display_housenumber(raw_housenumber text)
 RETURNS text AS $$
-DECLARE
-  min_number int;
-  max_number int;
-BEGIN
-  -- Check if the input string contains a semi-colon separator
-  IF raw_housenumber !~ ';' THEN
-    RETURN raw_housenumber;
-  END IF;
-
-  IF raw_housenumber ~ '[^0-9;]' THEN
-    RETURN display_housenumber_nonnumeric(raw_housenumber);
-  END IF;
-
-  -- Find the minimum and maximum numbers in the list
-  SELECT MIN(value), MAX(value) INTO min_number, max_number
-  FROM unnest(string_to_array(raw_housenumber, ';')::int[]) AS value;
-
-  -- Return the consolidated range string
-  RETURN min_number::text || '–' || max_number::text;
-END;
-$$ LANGUAGE plpgsql;
+  SELECT CASE
+    WHEN raw_housenumber !~ ';' THEN raw_housenumber
+    WHEN raw_housenumber ~ '[^0-9;]' THEN display_housenumber_nonnumeric(raw_housenumber)
+    ELSE
+      (SELECT min(value)::text || '–' || max(value)::text
+       FROM unnest(string_to_array(raw_housenumber, ';')::int[]) AS value)
+  END
+$$ LANGUAGE SQL IMMUTABLE;
