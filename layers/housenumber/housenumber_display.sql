ce688ccf87dcba2a2f2909278c951b4858e09c30
Brian Sperlongano <zelonewolf@gmail.com>
2023-07-06 23:20:04 -0400

    Handle non-numeric cases

--- layers/housenumber/housenumber_display.sql
+++ layers/housenumber/housenumber_display.sql
@@ -1,3 +1,17 @@
+CREATE OR REPLACE FUNCTION display_housenumber_nonnumeric(raw_housenumber text)
+RETURNS text AS $$
+DECLARE
+  arr text[];
+BEGIN
+  -- Convert the input string into an array of values
+  arr := string_to_array(raw_housenumber, ';')::text[];
+
+  -- Return the first and last value in the array
+  RETURN arr[1] || '–' || arr[array_length(arr, 1)];
+END;
+$$ LANGUAGE plpgsql;
+
+
 CREATE OR REPLACE FUNCTION display_housenumber(raw_housenumber text)
 RETURNS text AS $$
 DECLARE
@@ -7,6 +21,10 @@ BEGIN
   -- Check if the input string contains a semi-colon separator
   IF raw_housenumber !~ ';' THEN
     RETURN raw_housenumber;
+  END IF;
+
+  IF raw_housenumber ~ '[^0-9;]' THEN
+    RETURN display_housenumber_nonnumeric(raw_housenumber);
   END IF;
 
   -- Find the minimum and maximum numbers in the list
