aef44a597483de10381f51fe231bd98e6cafb3ce
Brian Sperlongano <zelonewolf@gmail.com>
2023-07-06 22:51:54 -0400

    Conslidate code

--- layers/housenumber/housenumber_display.sql
+++ layers/housenumber/housenumber_display.sql
@@ -1,27 +1,19 @@
 CREATE OR REPLACE FUNCTION display_housenumber(raw_housenumber text)
 RETURNS text AS $$
 DECLARE
-  numbers_arr int[];
   min_number int;
   max_number int;
-  consolidated_range text;
 BEGIN
   -- Check if the input string contains a semi-colon separator
   IF raw_housenumber !~ ';' THEN
     RETURN raw_housenumber;
   END IF;
 
-  -- Convert the input string into an array of integers
-  numbers_arr := string_to_array(raw_housenumber, ';')::int[];
-
-  -- Find the minimum and maximum numbers in the array
+  -- Find the minimum and maximum numbers in the list
   SELECT MIN(value), MAX(value) INTO min_number, max_number
-  FROM unnest(numbers_arr) AS value;
-
-  -- Build the consolidated range string
-  consolidated_range := min_number::text || '–' || max_number::text;
+  FROM unnest(string_to_array(raw_housenumber, ';')::int[]) AS value;
 
   -- Return the consolidated range string
-  RETURN consolidated_range;
+  RETURN min_number::text || '–' || max_number::text;
 END;
 $$ LANGUAGE plpgsql;
