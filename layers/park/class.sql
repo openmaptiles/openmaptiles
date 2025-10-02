1e37a8bdcd69e0807c148f4e31a573a69cd932aa
Brian Sperlongano <zelonewolf@gmail.com>
2025-10-02 00:18:06 -0400

    Add protected_area handling

--- layers/park/class.sql
+++ layers/park/class.sql
@@ -1,9 +1,15 @@
-CREATE OR REPLACE FUNCTION park_class(boundary text, leisure text, landuse text, historic text, maritime boolean)
+CREATE OR REPLACE FUNCTION park_class(boundary text, leisure text, landuse text, historic text, maritime boolean, tags hstore)
     RETURNS text AS
 $$
 SELECT CASE 
     WHEN maritime = TRUE THEN 'marine'
     WHEN boundary = 'national_park' THEN 'national_park'
+    WHEN boundary = 'protected_area' THEN 
+        COALESCE(
+            NULLIF(tags->'protected_area', ''),
+            NULLIF(tags->'protection_title', ''),
+            'protected_area'
+        )
     WHEN leisure = 'nature_reserve' THEN 'nature_reserve'
     WHEN landuse = 'recreation_ground' THEN 'recreation_ground'
     WHEN historic <> '' THEN 'historic'
